98cd4e6a990fdfe00240eed0d17977d3629d9e30
dkoeplin <dkoeplinger@gmail.com>
2017-01-31 19:29:37 -0800

    Moved unit tests

--- apps/src/UnitTests.scala
+++ apps/src/UnitTests.scala
@@ -5,30 +5,128 @@ object SimpleSequential extends SpatialApp {
   import IR._
 
   def simpleSeq(xIn: Int, yIn: Int): Int = {
-    val innerPar = 1 (1 -> 1 -> 1)
-    val tileSize = 96 (96 -> 96 -> 96)
+    val innerPar = 1 (1 -> 1)
+    val tileSize = 96 (96 -> 96)
 
     val x = ArgIn[Int]
     val y = ArgIn[Int]
-    val out = ArgIn[Int]
+    val out = ArgOut[Int]
     setArg(x, xIn)
     setArg(y, yIn)
-
     Accel {
-      val b1 = SRAM[Int](tileSize)
-      Sequential.Foreach(tileSize by 1 par innerPar){ii =>
-        b1(ii) = x.value * ii
+      val bram = SRAM[Int](tileSize)
+      Foreach(tileSize by 1 par innerPar){ ii =>
+        bram(ii) = x.value * ii
       }
-      out := b1(y)
+      out := bram(y.value)
     }
-
     getArg(out)
   }
 
+  @virtualize
   def main() {
     val x = args(0).to[Int]
     val y = args(1).to[Int]
     val result = simpleSeq(x, y)
 
+    val a1 = Array.tabulate(96){i => x * i}
+    val gold = a1(y)
+
+    println("expected: " + gold)
+    println("result:   " + result)
+    val chkSum = result == gold
+    assert(chkSum)
+    println("PASS: " + chkSum + " (SimpleSeq)")
   }
 }
+
+
+object DeviceMemcpy extends SpatialApp {
+  import IR._
+
+  val N = 192
+  type T = Int
+  def memcpyViaFPGA(srcHost: Array[T]): Array[T] = {
+    val fpgaMem = DRAM[Int](N)
+    setMem(fpgaMem, srcHost)
+
+    val y = ArgOut[Int]
+    Accel { y := 10 }
+
+    getMem(fpgaMem)
+  }
+
+  @virtualize
+  def main() {
+    val arraySize = N
+    val c = args(0).to[Int]
+
+    val src = Array.tabulate(arraySize){i => i*c }
+    val dst = memcpyViaFPGA(src)
+    println("Sent in: ")
+    for (i <- 0 until arraySize){ print(src(i) + " ") }
+    println("\nGot out: ")
+    for (i <- 0 until arraySize){ print(dst(i) + " ") }
+    println("")
+    val chkSum = dst.zip(src){_ == _}.reduce{_&&_}
+    println("PASS: " + chkSum + " (DeviceMemcpy)")
+  }
+}
+
+
+object SimpleTileLoadStore extends SpatialApp {
+  import IR._
+  type T = Int
+  val N = 192
+
+  def simpleLoadStore(srcHost: Array[T], value: Int) = {
+    val loadPar  = 1 (1 -> 1)
+    val storePar = 1 (1 -> 1)
+    val tileSize = 96 (96 -> 96)
+
+    val srcFPGA = DRAM[Int](N)
+    val dstFPGA = DRAM[Int](N)
+    setMem(srcFPGA, srcHost)
+
+    val size = ArgIn[Int]
+    val x = ArgIn[Int]
+    setArg(x, value)
+    setArg(size, N)
+    Accel {
+      val b1 = SRAM[Int](tileSize)
+      Sequential.Foreach(size by tileSize) { i =>
+        b1 := srcFPGA(i::i+tileSize)
+
+        val b2 = SRAM[SInt](tileSize)
+        Foreach(tileSize by 1) { ii =>
+          b2(ii) = b1(ii) * x
+        }
+
+        dstFPGA(i::i+tileSize) := b2
+      }
+      ()
+    }
+    getMem(dstFPGA)
+  }
+
+  @virtualize
+  def main() {
+    val arraySize = N
+    val value = args(0).to[Int]
+
+    val src = Array.tabulate[Int](arraySize) { i => i }
+    val dst = simpleLoadStore(src, value)
+
+    val gold = src.map { _ * value }
+
+    println("Sent in: ")
+    (0 until arraySize) foreach { i => print(gold(i) + " ") }
+    println("Got out: ")
+    (0 until arraySize) foreach { i => print(dst(i) + " ") }
+    println("")
+
+    val cksum = dst.zip(gold){_ == _}.reduce{_&&_}
+    println("PASS: " + cksum + " (SimpleTileLoadStore)")
+  }
+}
+
