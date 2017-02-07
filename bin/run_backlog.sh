c8cd44b1d49e4ae53b168611f0fe7dfb3d369bd0
mattfel1 <mattfel@stanford.edu>
2017-02-07 10:29:25 -0800

    add script for running bakclog of reg tests

---
+++ bin/run_backlog.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Check if you are on the stanford network
+if nc -z tucson.stanford.edu 22 2>/dev/null; then
+    echo "On Stanford network: ✓"
+    online=1
+else
+    echo "On Stanford network: ✗"
+    online=0
+fi
+
+files=(*)
+new_packets=()
+for f in ${files[@]}; do if [[ $f = *".new"* ]]; then new_packets+=($f); fi; done
+for f in ${new_packets[@]}; do
+	dst=`sed -n '12p' $f`
+	path=`sed -n '13p' $f`
+	if [[ $online = 1 ]]; then
+		scp $f ${dst}.stanford.edu:${path}
+	    echo "Copied packet $f to $dst.."
+	fi
+	sleep 0.1
+done
+
