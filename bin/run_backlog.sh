d461dce586d71563c06a576d7b6a2eb199d1549d
mattfel1 <mattfel@stanford.edu>
2017-02-11 11:50:00 -0800

    change emission flag for reg test

--- bin/run_backlog.sh
+++ bin/run_backlog.sh
@@ -3,7 +3,7 @@
 echo "This script is not yet tested!!"
 
 # Check if you are on the stanford network
-if nc -z tucson.stanford.edu 22 2>/dev/null; then
+if timeout 2 nc -z tucson.stanford.edu 22 2>/dev/null; then
     echo "On Stanford network: ✓"
     online=1
 else
@@ -13,13 +13,15 @@ fi
 
 files=(*)
 new_packets=()
-for f in ${files[@]}; do if [[ $f = *".new"* ]]; then new_packets+=($f); fi; done
+for f in ${files[@]}; do if [[ $f = *".networkerror"* ]]; then new_packets+=($f); fi; done
 for f in ${new_packets[@]}; do
 	dst=`sed -n '12p' $f`
 	path=`sed -n '13p' $f`
+	fnew=`echo $f | sed 's/networkerror/new/g'`
 	if [[ $online = 1 ]]; then
-		scp $f ${dst}.stanford.edu:${path}
+		scp $f ${dst}.stanford.edu:${path}/${fnew}
 	    echo "Copied packet $f to $dst.."
+	    # rm $f
 	fi
 	sleep 0.1
 done
