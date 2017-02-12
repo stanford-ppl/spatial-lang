cde2eccdb5bf9d378656cd1fd4a94269fe284160
mattfel1 <mattfel@stanford.edu>
2017-02-12 11:08:22 -0800

    add a grep

--- bin/regression_functions.sh
+++ bin/regression_functions.sh
@@ -495,7 +495,7 @@ cd ${5}/out
 make clean sim 2>&1 | tee -a ${5}/log
 
 # Check for crashes in backend compilation
-wc=\$(cat ${5}/log | \"recipe for target 'bitstream-sim' failed\" | wc -l)
+wc=\$(cat ${5}/log | grep \"recipe for target 'bitstream-sim' failed\" | wc -l)
 if [ \"\$wc\" -ne 0 ]; then
   report \"failed_compile_backend_crash\" \"[STATUS] Declaring failure compile_chisel chisel side\" 0
 fi
