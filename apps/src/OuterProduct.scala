55c0f4f9bf6d788c39a5176626562592fef87175
dkoeplin <dkoeplinger@gmail.com>
2017-02-15 19:47:40 -0800

    Fix outer product

--- apps/src/OuterProduct.scala
+++ apps/src/OuterProduct.scala
@@ -1,22 +1,19 @@
 import spatial._
 import org.virtualized._
 
-object OuterProduct extends SpatialApp {
+object OuterProduct extends SpatialApp { // Regression (Dense) // Args: 192 192
   import IR._
   type X = Int
 
-  val tileSize1 = 192
-  val tileSize2 = 192
+  val tileSize1 = 64
+  val tileSize2 = 64
   val op = 1
-  //val tileSize1 = 96
-  //val tileSize2 = 96
-  //val op = 8
   val ip = 1
 
   @virtualize
   def outerproduct[T:Staged:Num](a: Array[T], b: Array[T]) = {
-    val tileSizeA = tileSize1 (96 -> 96 -> 38400)
-    val tileSizeB = tileSize2 (96 -> 96 -> 38400)
+    val tileSizeA = tileSize1 (64 -> 64 -> 38400)
+    val tileSizeB = tileSize2 (64 -> 64 -> 38400)
     val outerPar  = op (1 -> 4)
     val innerPar  = ip (1 -> 256)
 
@@ -40,28 +37,6 @@ object OuterProduct extends SpatialApp {
         val b1 = SRAM[T](tileSizeA)
         val b2 = SRAM[T](tileSizeB)
         val outTile = SRAM[T](tileSizeA, tileSizeB)
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-        Foreach(tileSizeA by 1, tileSizeB par innerPar){ (ii,jj) => outTile(ii, jj) = b1(ii) * b2(jj) }
-
-        out(i::i+tileSizeA, j::j+tileSizeB) store outTile
-        // val blkA = Reg[Int]
-        // val blkB = Reg[Int]
-        // Parallel {
-        //   b1 load vec1(i::i+tileSizeA)
-        //   b2 load vec2(j::j+tileSizeB)
-        //   Pipe{ blkA := min(sizeA - i, tileSizeA) }
-        //   Pipe{ blkB := min(sizeB - j, tileSizeB) }
-        // }
-        // Foreach(blkA by 1, blkB par innerPar){ (ii,jj) => outTile(ii, jj) = b1(ii) * b2(jj) } // 2
-
-        // out(i::i+blkA, j::j+blkB) store outTile
-=======
-=======
->>>>>>> origin/master
-=======
->>>>>>> origin/master
         //val blkA = Reg[Int]
         //val blkB = Reg[Int]
         Parallel {
@@ -73,13 +48,6 @@ object OuterProduct extends SpatialApp {
         Foreach(tileSizeA by 1, tileSizeB par innerPar){ (ii,jj) => outTile(ii, jj) = b1(ii) * b2(jj) } // 2
 
         out(i::i+tileSizeA, j::j+tileSizeB) store outTile
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> origin/master
-=======
->>>>>>> origin/master
-=======
->>>>>>> origin/master
       }
     }
     getMem(out)
@@ -91,17 +59,19 @@ object OuterProduct extends SpatialApp {
     val N = args(1).to[Int]
     // val a = Array.fill(M)(random[T](100))
     // val b = Array.fill(N)(random[T](100))
-    val a = Array.tabulate(M) { i => i.to[X] }
-    val b = Array.fill(N){ 1.as[X] }
+    val a = Array.tabulate(M) { i => (i % 256).to[X] }
+    val b = Array.tabulate(N){ i => (i % 256).to[X] }
 
     val result = outerproduct(a, b)
 
     val gold = Array.tabulate(M){i => Array.tabulate(N){j => a(i) * b(j) }}.flatten
-    println("expected cksum: " + gold.map(a => a).reduce{_+_})
-    println("result cksum:   " + result.map(a => a).reduce{_+_})
-    (0 until M*N) foreach { i => assert(result(i) == gold(i)) }
+    val gold_cksum = gold.map(a => a).reduce{_+_}
+    val result_cksum = result.map(a => a).reduce{_+_}
+    println("expected cksum: " + gold_cksum)
+    println("result cksum:   " + result_cksum)
+    // (0 until M*N) foreach { i => assert(result(i) == gold(i)) }
 
-    val cksum = result.zip(gold){_ == _}.reduce{_&&_}
+    val cksum = result_cksum == gold_cksum
     println("PASS: " + cksum + " (OuterProduct)")
 
 
