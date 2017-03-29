e00e27c99c5f42439041e26f516a4f4bef2e91b9
Matthew Feldman <mattfel@stanford.edu>
2017-03-29 15:59:37 -0700

    fix negative fixpt mems

--- apps/src/Convolution_FPGA.scala
+++ apps/src/Convolution_FPGA.scala
@@ -91,6 +91,9 @@ object Convolution_FPGA extends SpatialApp {
     // val image = (0::R, 0::C){(i,j) => if (j > 3 && i > 3 && j < 11 && i < 11) 256 else 0 }
     val image = (0::R, 0::C){(i,j) => if (j > border && j < C-border && i > border && i < C - border) i*16 else 0}
 
+    val kh = List((List(1,2,1), List(0,0,0), List(-1,-2,-1)))
+    val kv = List((List(1,0,-1), List(2,0,-2), List(1,0,-1)))
+
     val output = convolve(image)
 
     /*
@@ -104,7 +107,18 @@ object Convolution_FPGA extends SpatialApp {
       1   0  -1
 
     */
-    val gold = (0::R, 0::C){(i,j) => if (j > border && j < C-border && i > border && i < C - border) (i*16*4 + (i+2)*16*4) else 0 }
+    val gold = (0::R, 0::C){(i,j) => 
+      val px00 = if (j > border && j < C-border && i > border && i < C - border) i*16 else 0
+      val px01 = if ((j+1) > border && (j+1) < C-border && i > border && i < C - border) i*16 else 0
+      val px02 = if ((j+2) > border && (j+2) < C-border && i > border && i < C - border) i*16 else 0
+      val px10 = if (j > border && j < C-border && (i+1) > border && (i+1) < C - border) (i+1)*16 else 0
+      val px11 = if ((j+1) > border && (j+1) < C-border && (i+1) > border && (i+1) < C - border) (i+1)*16 else 0
+      val px12 = if ((j+2) > border && (j+2) < C-border && (i+1) > border && (i+1) < C - border) (i+1)*16 else 0
+      val px20 = if (j > border && j < C-border && (i+2) > border && (i+2) < C - border) (i+2)*16 else 0
+      val px21 = if ((j+1) > border && (j+1) < C-border && (i+2) > border && (i+2) < C - border) (i+2)*16 else 0
+      val px22 = if ((j+2) > border && (j+2) < C-border && (i+2) > border && (i+2) < C - border) (i+2)*16 else 0
+      px00 * 2 + px01 * 2 + px10 * 2 + px12 * 2 + px21 * 2 + px22 * 2
+    }
 
     printMatrix(image, "Image")
     printMatrix(gold, "Gold")
