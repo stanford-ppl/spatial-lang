93e88641fb08f0cb12b720e59d32470cfac80d6d
raghu <raghup17@gmail.com>
2017-04-03 15:27:11 -0700

    Use file IO to simulate input and output streams. File names are temporarily hardcoded. File names will have to be passed to the simulator from the host program.

--- src/spatial/codegen/chiselgen/resources/template-level/fringeVCS/Streams.h
+++ src/spatial/codegen/chiselgen/resources/template-level/fringeVCS/Streams.h
@@ -22,7 +22,17 @@ public:
   string filename;
   int fd;
 
+  uint32_t next;
+  bool lastEncountered;
+
   InputStream(string filename) : filename(filename) {
+    fd = open(filename.c_str(), O_RDONLY);
+    ASSERT(fd != -1, "[InputStream] Error opening file '%s': %s\n", filename.c_str(), strerror(errno));
+    next = 0;
+    lastEncountered = false;
+  }
+
+  void updateNext() {
 
   }
 
@@ -30,10 +40,24 @@ public:
   // - New data exists: Read and pass to writeStream
   // - No new data: Return
   void send() {
-    static uint32_t data = 0;
-    static uint32_t tag = 0xF00DCAFE;
-    uint32_t last = 0;
-    writeStream(data++, tag, last);
+    uint32_t data = 0;
+    static uint32_t tag = 0;
+    bool last = false;
+
+    int bytesread = read(fd, &data, sizeof(uint32_t));
+    ASSERT(bytesread >= 0, "[InputStream] Error reading file '%s': %s\n", filename.c_str(), strerror(errno));
+    if (bytesread < sizeof(uint32_t)) {
+      if (lastEncountered) { // If we have already seen last, don't send it again
+        last = false;
+      } else {
+        last = true;
+        lastEncountered = true;
+      }
+    } else {
+      last = false;
+    }
+
+    writeStream(data, tag++, last);
   }
 
   ~InputStream() {
@@ -48,15 +72,19 @@ public:
   int fd;
 
   OutputStream(string filename) : filename(filename) {
-
+    fd = open(filename.c_str(), O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+    ASSERT(fd != -1, "[OutputStream] Error opening file '%s': %s\n", filename.c_str(), strerror(errno));
   }
 
   // Callback function called from SV -> sim.cpp
   // Just write data into a file
   // [TODO] In current scheme, output stream is always ready. Model backpressure more accurately.
   void recv(uint32_t udata, uint32_t utag, bool blast) {
-    // Currently just print read data out to console
+    // Currently print read data out to console in addition to file
     EPRINTF("[readOutputStream] data = %08x, tag = %08x, last = %x\n", udata, utag, blast);
+
+    int bytes = write(fd, &udata, sizeof(uint32_t));   // Ignore tag for now
+    ASSERT(bytes >= 0, "[OutputStream] Error writing to file '%s': %s\n", filename.c_str(), strerror(errno));
   }
 
   ~OutputStream() {
@@ -72,7 +100,7 @@ OutputStream *outStream = NULL;
 
 void initStreams() {
   // Initialize simulation streams
-  inStream = new InputStream("");
-  outStream = new OutputStream("");
+  inStream = new InputStream("in.txt");
+  outStream = new OutputStream("out.txt");
 }
 
