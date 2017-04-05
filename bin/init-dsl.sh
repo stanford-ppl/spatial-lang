82f174826560df6a20a2137340d2ec4e3a1196ce
dkoeplin <dkoeplinger@gmail.com>
2017-04-05 15:11:51 -0700

    fix init-dsl

--- bin/init-dsl.sh
+++ bin/init-dsl.sh
@@ -6,17 +6,19 @@ if [ "$SPATIAL_HOME" == "" ]; then
     SPATIAL_HOME=$(dirname $(dirname $(readlink -f "$0")))
 fi
 
+echo $SPATIAL_HOME
+
 if [ $1 = 1 ]; then #requested just dsl
   cd ${SPATIAL_HOME}
-  sbt "spatial/assembly"
+  sbt "; project apps; assembly"
 fi
 
 if [ $1 = 2 ]; then # requested apps
   cd ${SPATIAL_HOME}
-  sbt "apps/assembly"
+  sbt "; project apps; assembly"
 fi
 
 if [ $1 = 3 ]; then # jar release
   cd ${SPATIAL_HOME}
-  sbt "apps/assembly"
+  sbt "; project apps; assembly"
 fi
