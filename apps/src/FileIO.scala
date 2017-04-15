efe1496351566ff0eae90e5f2a558a87e6f15a65
Ruben Fiszel <ruben.fiszel@epfl.ch>
2017-04-15 12:26:37 -0700

    fixed implicit ops

--- apps/src/FileIO.scala
+++ apps/src/FileIO.scala
@@ -1,34 +1,41 @@
 import spatial._
 import org.virtualized._
 
-object CSV1D extends SpatialApp { 
+object CSV1D extends SpatialApp {
+
   import IR._
 
   @virtualize
   def main() {
-    type T = FixPt[TRUE,_16,_16]
+    type T = FixPt[TRUE, _16, _16]
     val tilesize = 16
-    val data = loadCSV1D[T]("/home/mattfel/spatial-lang/spatial/core/resources/cppgen/testdata/1d.csv", ",") 
+    val data = loadCSV1D[T]("/home/mattfel/spatial-lang/spatial/core/resources/cppgen/testdata/1d.csv", ",")
     val memsize = ArgIn[Int]
     setArg(memsize, data.length.to[Int])
     val srcmem = DRAM[T](memsize)
     setMem(srcmem, data)
     val result = ArgOut[T]
 
-    Accel{
+    Accel {
       val fpgamem = SRAM[T](tilesize)
-      result := Reduce(Reg[T](0.to[T]))(memsize.value by tilesize) {r => 
-        fpgamem load srcmem(r::r+tilesize)
-        Reduce(Reg[T](0.to[T]))(tilesize by 1) { i => 
+      result := Reduce(Reg[T](0.to[T]))(memsize.value by tilesize) { r =>
+        fpgamem load srcmem(r :: r + tilesize)
+        Reduce(Reg[T](0.to[T]))(tilesize by 1) { i =>
           fpgamem(i)
-        }{ _ + _ }
-      }{_ + _}
+        } {
+          _ + _
+        }
+      } {
+        _ + _
+      }
     }
 
 
     val r = getArg(result)
 
-    val gold = data.reduce{_+_}
+    val gold = data.reduce {
+      _ + _
+    }
 
     printArray(data)
     println("Gold sum is " + gold)
@@ -38,34 +45,41 @@ object CSV1D extends SpatialApp {
   }
 }
 
-object SSV1D extends SpatialApp { 
+object SSV1D extends SpatialApp {
+
   import IR._
 
   @virtualize
   def main() {
-    type T = FixPt[TRUE,_16,_16]
+    type T = FixPt[TRUE, _16, _16]
     val tilesize = 16
-    val data = loadCSV1D[T]("/home/mattfel/spatial-lang/spatial/core/resources/cppgen/testdata/1d.ssv", " ") 
+    val data = loadCSV1D[T]("/home/mattfel/spatial-lang/spatial/core/resources/cppgen/testdata/1d.ssv", " ")
     val memsize = ArgIn[Int]
     setArg(memsize, data.length.to[Int])
     val srcmem = DRAM[T](memsize)
     setMem(srcmem, data)
     val result = ArgOut[T]
 
-    Accel{
+    Accel {
       val fpgamem = SRAM[T](tilesize)
-      result := Reduce(Reg[T](0.to[T]))(memsize.value by tilesize) {r => 
-        fpgamem load srcmem(r::r+tilesize)
-        Reduce(Reg[T](0.to[T]))(tilesize by 1) { i => 
+      result := Reduce(Reg[T](0.to[T]))(memsize.value by tilesize) { r =>
+        fpgamem load srcmem(r :: r + tilesize)
+        Reduce(Reg[T](0.to[T]))(tilesize by 1) { i =>
           fpgamem(i)
-        }{ _ + _ }
-      }{_ + _}
+        } {
+          _ + _
+        }
+      } {
+        _ + _
+      }
     }
 
 
     val r = getArg(result)
 
-    val gold = data.reduce{_+_}
+    val gold = data.reduce {
+      _ + _
+    }
 
     printArray(data)
     println("Gold sum is " + gold)
@@ -75,12 +89,13 @@ object SSV1D extends SpatialApp {
   }
 }
 
-object SSV2D extends SpatialApp { 
+object SSV2D extends SpatialApp {
+
   import IR._
 
   @virtualize
   def main() {
-    type T = FixPt[TRUE,_16,_16]
+    type T = FixPt[TRUE, _16, _16]
     val rowtile = 2
     val coltile = 16
     val data = loadCSV2D[T]("/home/mattfel/spatial-lang/spatial/core/resources/cppgen/testdata/2d.ssv", " ", "\n")
@@ -95,22 +110,28 @@ object SSV2D extends SpatialApp {
     println(data.rows + " x " + data.cols + " matrix:")
     printMatrix(data)
 
-    Accel{
+    Accel {
       val fpgamem = SRAM[T](rowtile, coltile)
 
-      result := Reduce(Reg[T](0.to[T]))(memrows.value by rowtile, memcols.value by coltile) {(r, c) => 
-        fpgamem load srcmem(r::r+rowtile, c::c+coltile)
-        Reduce(Reg[T](0.to[T]))(rowtile by 1, coltile by 1) { (i,j) => 
-          fpgamem(i,j)
-        }{ _ + _ }
-      }{_ + _}
+      result := Reduce(Reg[T](0.to[T]))(memrows.value by rowtile, memcols.value by coltile) { (r, c) =>
+        fpgamem load srcmem(r :: r + rowtile, c :: c + coltile)
+        Reduce(Reg[T](0.to[T]))(rowtile by 1, coltile by 1) { (i, j) =>
+          fpgamem(i, j)
+        } {
+          _ + _
+        }
+      } {
+        _ + _
+      }
 
     }
 
 
     val r = getArg(result)
 
-    val gold = data.reduce{_+_}
+    val gold = data.reduce {
+      _ + _
+    }
 
     println("Gold sum is " + gold)
     println("Accel sum is " + r)
