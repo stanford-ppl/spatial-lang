d8c327edb667d3b1058b6de000286d9073060f8a
Matthew Feldman <mattfel@stanford.edu>
2017-04-21 11:24:47 -0700

    add rough script for binding two branches together quickly.  Not really tested much though

---
+++ bin/ezgit.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+# Script for quickly merging two branches into one-another.  For example
+#   branch.sh develop fpga will merge develop into fpga branch, then merge
+#   the fpga into develop.  Call this script from your spatial-lang home
+
+# Usage: 
+#   arg 1 = upper branch name (develop, pre-master, master, etc..)
+#   arg 2 = lower branch name
+#   arg 3 = Run regression on higher branch? (1 or 0)
+
+
+
+
+# Clear git merging log
+rm /tmp/pub
+
+cd scala-virtualized
+git pull
+virtbranch=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
+if [[ $virtbranch != "argon" ]]; then
+	echo "Why are you on $virtbranch branch instead of argon branch for scala-virtualized?"
+	exit 1
+fi
+
+
+# Get current branch
+currentbranch=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
+
+# Checkout lower branch
+cd ../argon
+git checkout $2
+cd ..
+git checkout $2
+
+# Merge higher into lower
+cd argon
+git merge origin/$1 | tee -a /tmp/pub
+error=(`cat /tmp/pub | grep "CONFLICT" | wc -l`)
+if [[ $error != 0 ]]; then
+	echo "Conflict error merging $1 into $2.  Please resolve"
+	exit 1
+fi
+sleep 1
+git push
+
+cd ../
+git merge origin/$1 | tee -a /tmp/pub
+error=(`cat /tmp/pub | grep "CONFLICT" | wc -l`)
+if [[ $error != 0 ]]; then
+	echo "Conflict error merging $1 into $2.  Please resolve"
+	exit 1
+fi
+sleep 1
+git add argon
+git add scala-virtualized
+git commit -m "auto merge"
+git push
+
+
+# Merge lower into higher
+cd argon
+git checkout $1
+git pull
+git merge origin/$2 | tee -a /tmp/pub
+error=(`cat /tmp/pub | grep "CONFLICT" | wc -l`)
+if [[ $error != 0 ]]; then
+	echo "Conflict error merging $1 into $2.  Please resolve"
+	exit 1
+fi
+sleep 1
+git push
+
+cd ../
+git checkout $1
+git pull
+git merge origin/$2 | tee -a /tmp/pub
+error=(`cat /tmp/pub | grep "CONFLICT" | wc -l`)
+if [[ $error != 0 ]]; then
+	echo "Conflict error merging $1 into $2.  Please resolve"
+	exit 1
+fi
+sleep 1
+git add argon
+git add scala-virtualized
+git commit -m "auto merge"
+git push
+
+# Regression test
+if [[ $3 = 1 ]]; then
+	bash bin/run_regression.sh
+fi
+
+# Go back to your original branch
+cd argon
+git checkout $currentbranch
+cd ..
+git checkout $currentbranch
