09492071ce30c7bb5a2c8dbee53546a672052d63
Yaqi Zhang <hsbzyq@gmail.com>
2017-04-24 22:04:45 -0700

    fixes to pircodegen and optimizer

--- spatial/core/src/spatial/codegen/pirgen/PIROptimizer.scala
+++ spatial/core/src/spatial/codegen/pirgen/PIROptimizer.scala
@@ -126,7 +126,7 @@ trait PIROptimizer extends PIRTraversal {
     cu.computeStages.foreach{stage => dbgs(s"  $stage") }
 
     val bypassStages = cu.computeStages.flatMap{
-      case bypass@MapStage(PIRBypass, List(LocalRef(_,MemLoadReg(mem:CUMemory))), List(LocalRef(_,VectorOut(out: VectorBus)))) if mem.writePort.isDefined =>
+      case bypass@MapStage(PIRBypass, List(LocalRef(_,MemLoadReg(mem:CUMemory))), List(LocalRef(_,VectorOut(out: VectorBus)))) if mem.writePort.isDefined & mem.mode==VectorFIFOMode =>
         val in = mem.writePort.get
         if (isInterCU(out)) {
           dbgs(s"Found route-thru: $in -> $out")
@@ -135,7 +135,7 @@ trait PIROptimizer extends PIRTraversal {
         }
         else None
 
-      case bypass@MapStage(PIRBypass, List(LocalRef(_,MemLoadReg(mem:CUMemory))), List(LocalRef(_,ScalarOut(out: OutputArg)))) if mem.writePort.isDefined =>
+      case bypass@MapStage(PIRBypass, List(LocalRef(_,MemLoadReg(mem:CUMemory))), List(LocalRef(_,ScalarOut(out: OutputArg)))) if mem.writePort.isDefined & (mem.mode==ScalarFIFOMode | mem.mode==ScalarBufferMode)=>
         val in = mem.writePort.get
         if (isInterCU(in)) {
           dbgs(s"Found route-thru: $in -> $out")
@@ -144,7 +144,7 @@ trait PIROptimizer extends PIRTraversal {
         }
         else None
 
-      case bypass@MapStage(PIRBypass, List(LocalRef(_,MemLoadReg(mem:CUMemory))), List(LocalRef(_,VectorOut(out: VectorBus)))) if mem.writePort.isDefined =>
+      case bypass@MapStage(PIRBypass, List(LocalRef(_,MemLoadReg(mem:CUMemory))), List(LocalRef(_,VectorOut(out: VectorBus)))) if mem.writePort.isDefined & mem.mode==VectorFIFOMode=>
         val in = mem.writePort.get
         cus.find{cu => in match {
             case in:VectorBus =>
