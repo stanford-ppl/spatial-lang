55597cee331fecb0f5d3dafbe725dc70653eadc1
Yaqi Zhang <hsbzyq@gmail.com>
2017-05-01 18:00:20 -0700

    new results for GDA and SGD

---
+++ apps/src/LogReg_5_1536_ip_16_ts_65_dim_384_op_11.scala
@@ -0,0 +1,133 @@
+import spatial._
+import org.virtualized._
+
+object LogReg_5_1536_ip_16_ts_65_dim_384_op_11 extends SpatialApp {
+  import IR._
+
+  type X = FixPt[TRUE,_16,_16]
+
+  val margin = 5
+val dim = 384
+  val D = dim
+  val A = 1
+
+val ip = 16
+val op = 11
+
+val ts = 65
+
+  //def sigmoid[T:Type:Num](t:T) = 1.to[T]/(exp(-t) + 1.to[T])
+  def sigmoid[T:Type:Num](t:T) = 1.to[T]/(abs(-t) + 1.to[T])
+
+  @virtualize
+  def logreg[T:Type:Num](xIn: Array[T], yIn: Array[T], tt: Array[T], n: Int, it: Int) = {
+    val iters = ArgIn[Int]
+    val N     = ArgIn[Int]
+    setArg(iters, it)
+    setArg(N, n)
+
+    val BN = ts (96 -> 96 -> 9600)
+    val PX = 1 (1 -> 1)
+    val P1 = ip (1 -> 2)
+    val P2 = ip (1 -> 96)
+    val P3 = op (1 -> 96)
+
+    val x = DRAM[T](N, D)
+    val y = DRAM[T](N)
+    val theta = DRAM[T](D)
+
+    setMem(x, xIn)
+    setMem(y, yIn)
+    setMem(theta, tt)
+
+    Accel {
+      val btheta = SRAM[T](D)
+
+      Sequential.Foreach(iters by 1) { epoch =>
+
+        Sequential.MemReduce(btheta)(1 by 1){ xx =>
+          val gradAcc = SRAM[T](D)
+          Foreach(N by BN){ i =>
+            val logregX = SRAM[T](BN, D)
+            val logregY = SRAM[T](BN)
+            Parallel {
+              logregX load x(i::i+BN, 0::D par P2)
+              logregY load y(i::i+BN par P2)
+            }
+            MemReduce(gradAcc)(BN par P3){ ii =>
+              val pipe2Res = Reg[T]
+              val subRam   = SRAM[T](D)
+
+              val dotAccum = Reduce(Reg[T])(D par P2){j => logregX(ii,j) * btheta(j) }{_+_}  // read
+              Pipe { pipe2Res := (logregY(ii) - sigmoid(dotAccum.value)) }
+              Foreach(D par P2) {j => subRam(j) = logregX(ii,j) - pipe2Res.value }
+              subRam
+            }{_+_}
+          }
+          gradAcc
+        }{(b,g) => b+g*A.to[T]}
+
+        // Flush gradAcc
+        //Pipe(D by 1 par P2) { i => gradAcc(i) = 0.to[T]}
+      }
+      theta(0::D par P2) store btheta // read
+    }
+    getMem(theta)
+  }
+
+  @virtualize
+  def main() {
+    val iters = args(0).to[Int]
+    val N = args(1).to[Int]
+
+    val sX = Array.fill(N){ Array.fill(D){ random[X](10.to[X])} }
+    val sY = Array.tabulate(N){ i => i.to[X]} //fill(N)( random[T](10.0) )
+    val theta = Array.fill(D) {random[X](1.to[X]) }
+
+    val result = logreg(sX.flatten,sY, theta, N, iters)
+
+    val gold = Array.empty[X](D)
+    val ids = Array.tabulate(D){i => i}
+    for (i <- 0 until D) {
+      gold(i) = theta(i)
+    }
+    for (i <- 0 until iters) {
+      val next = sX.zip(sY) {case (row, y) =>
+        // println("sigmoid for " + y + " is " + sigmoid(row.zip(gold){_*_}.reduce{_+_}))
+        val sub = y - sigmoid(row.zip(gold){(a,b) =>
+          // println("doing " + a + " * " + b + " on row " + y)
+          a*b}.reduce{_+_})
+        row.map{a =>
+          // println("subtraction for " + y + " is " + (a - sub))
+          a - sub}
+      }.reduce{(a,b) => a.zip(b){_+_}}
+      for (i <- 0 until D) {
+        gold(i) = gold(i) + next(i)
+      }
+      // printArr(gold, "gold now")
+    }
+
+
+    printArray(gold, "gold: ")
+    printArray(result, "result: ")
+
+    val cksum = result.zip(gold){ (a,b) => a > b-margin && a < b+margin}.reduce{_&&_}
+    // println("max err: " + result.zip(gold){(a,b) => (a-b)*(a-b)}.reduce{Math.max(_,_)})
+    // println("mean err: " + result.zip(gold){(a,b) => (a-b)*(a-b)}.reduce{_+_} / D)
+    println("PASS: " + cksum  + " (LogReg)")
+
+
+
+    /* OptiMl version
+    val w = untilconverged(theta, maxIter = 30) { (cur, iter) =>
+      val gradient = ((0::x.numRows) { i =>
+        x(i)*(y(i) - sigmoid(cur *:* x(i)))
+      }).sum
+      val z = cur + gradient*alpha
+      z
+    }
+    */
+
+  }
+
+}
