5eaa61a92b599f87dec8c3e7528f067c4facf082
Matthew Feldman <mattfel@stanford.edu>
2017-05-05 12:38:38 -0700

    fix portland filling up in reg. Fix semicolons in some cpp math

--- spatial/core/src/spatial/codegen/cppgen/CppGenMath.scala
+++ spatial/core/src/spatial/codegen/cppgen/CppGenMath.scala
@@ -38,16 +38,16 @@ trait CppGenMath extends CppCodegen {
 
     case FltAbs(x)  => emit(src"${lhs.tp} $lhs = fabs($x);")
     case FltLog(x)  => x.tp match {
-      case DoubleType() => emit(src"${lhs.tp} $lhs = log($x)")
-      case FloatType()  => emit(src"${lhs.tp} $lhs = log($x)")
+      case DoubleType() => emit(src"${lhs.tp} $lhs = log($x);")
+      case FloatType()  => emit(src"${lhs.tp} $lhs = log($x);")
     }
     case FltExp(x)  => x.tp match {
-      case DoubleType() => emit(src"${lhs.tp} $lhs = exp($x)")
-      case FloatType()  => emit(src"${lhs.tp} $lhs = exp($x)")
+      case DoubleType() => emit(src"${lhs.tp} $lhs = exp($x);")
+      case FloatType()  => emit(src"${lhs.tp} $lhs = exp($x);")
     }
     case FltSqrt(x) => x.tp match {
-      case DoubleType() => emit(src"${lhs.tp} $lhs = sqrt($x)")
-      case FloatType()  => emit(src"${lhs.tp} $lhs = sqrt($x)")
+      case DoubleType() => emit(src"${lhs.tp} $lhs = sqrt($x);")
+      case FloatType()  => emit(src"${lhs.tp} $lhs = sqrt($x);")
     }
 
     case FltPow(x,exp) =>
