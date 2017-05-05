7d81efe061d03339fbdb4867b69a7a5bab83346e
dkoeplin <dkoeplinger@gmail.com>
2017-05-04 20:13:38 -0700

    update frame streaming example

--- apps/src/FrameStreaming.scala
+++ apps/src/FrameStreaming.scala
@@ -6,39 +6,20 @@ object FrameStreaming extends SpatialApp {
   import IR._
 
   override val target = DE1
+  val Cmax = 320
+  val Rmax = 240
 
-  val Kh = 3
-  val Kw = 3
-  val Cmax = 512
-  val Rmax = 512
-
-  type Int12 = FixPt[TRUE,_12,_0]
-  @struct case class Pixel24(b: UInt8, g: UInt8, r: UInt8)
   @struct case class Pixel16(b: UInt5, g: UInt6, r: UInt5)
 
   @virtualize
-  def convolveVideoStream(rows: Int, cols: Int): Unit = {
-    val R = ArgIn[Int]
-    val C = ArgIn[Int]
-    setArg(R, rows)
-    setArg(C, cols)
-
-    val imgIn  = StreamIn[Pixel24](target.VideoCamera)
+  def convolveVideoStream(): Unit = {
     val imgOut = BufferedOut[Pixel16](target.VGA)
 
     Accel {
-      val fifoIn = FIFO[Int12](128)
-
       Stream(*) { _ =>
-        Pipe {
-          val pixel = imgIn.value()
-          val grayPixel = (pixel.b.to[Int12] + pixel.g.to[Int12] + pixel.r.to[Int12]) / 3
-          fifoIn.enq(grayPixel)
-        }
-
-        Foreach(0 until R, 0 until C) { (r, c) =>
-          val pixel = fifoIn.deq()
-          imgOut(r, c) = Pixel16(pixel.to[UInt5], pixel.to[UInt6], pixel.to[UInt5])
+        Foreach(0 until Rmax, 0 until Cmax){ (r, c) =>
+          val pixel = mux(r > 60 && r < 120 && c > 60 && c < 120, Pixel16(0,63,0), Pixel16(0,0,0))
+          imgOut(r, c) = pixel
         }
       }
 
@@ -50,6 +31,6 @@ object FrameStreaming extends SpatialApp {
   def main() {
     val R = args(0).to[Int]
     val C = Cmax
-    convolveVideoStream(R, C)
+    convolveVideoStream()
   }
 }
