946d5bfc7c1eb3136d19f3c63601621cfc14e358
Matthew Feldman <mattfel@stanford.edu>
2017-05-11 20:10:42 -0700

    fix merging woes

--- apps/src/TPCH.scala
+++ apps/src/TPCH.scala
@@ -34,7 +34,7 @@ WHERE
 
 */
 
-object TPCHQ6 extends SpatialApp { // Regression (Dense) // Args: 384
+object TPCHQ6 extends SpatialApp { // Regression (Dense) // Args: 3840
   import IR._
 
   type FT = Int
@@ -46,9 +46,9 @@ object TPCHQ6 extends SpatialApp { // Regression (Dense) // Args: 384
   val margin = 1
 
   val innerPar = 16
-  val outerPar = 8
+  val outerPar = 2
 
-  val tileSize = 4000
+  val tileSize = 384
 
   @virtualize
   def tpchq6[T:Type:Num](datesIn: Array[Int], quantsIn: Array[Int], disctsIn: Array[T], pricesIn: Array[T]): T = {
