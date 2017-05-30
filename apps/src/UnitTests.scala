b0307c1ec9d1f8c041afb7340eed28389b3c0432
dkoeplin <dkoeplinger@gmail.com>
2017-05-29 23:39:28 -0700

    Move DocTests - why was it in the top directory??

--- apps/src/UnitTests.scala
+++ apps/src/UnitTests.scala
@@ -2278,3 +2278,30 @@ object NestedIfs extends SpatialApp {
     println("result:   " + result)
   }
 }
+
+object Tup2Test extends SpatialApp {
+  import IR._
+
+  @virtualize
+  def foo() : Int = {
+    type Tup = Tup2[Int, Int]
+    val out = ArgOut[Int]
+    val dr = DRAM[Tup](10)
+    Accel {
+      val s = SRAM[Tup](10)
+      s(5) = pack(42, 43)
+      dr(0::10) store s
+
+      val s1 = SRAM[Tup](10)
+      s1 load dr(0::10)
+      out := s1(5)._1 * s1(5)._2
+    }
+    getArg(out)
+  }
+
+  @virtualize
+  def main() {
+    val result = foo()
+    println(result)
+  }
+}
