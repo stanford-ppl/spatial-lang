f0064065b0dcacdbe02860fb001cdd7744c08551
kelayamatoz <zhaotian1991713@gmail.com>
2017-06-16 16:16:23 -0700

    LSTM, sigmoid being fussy

--- apps/src/LSTMForward.scala
+++ apps/src/LSTMForward.scala
@@ -9,10 +9,11 @@ import org.virtualized._
  * W_i \in D_h*d     : weights matrix to condition x_t
  * h_{t-1} \in D_h   : D_h is the size of hidden layer
  * U^i \in D_h * D_h : weights matrix to condition the previous hidden state 
+ *
  * Forward network:
- * i_t = sigmoid(W^i x_t + U^i h_{t-1})
- * f_t = sigmoid(W^f x_t + U^f h_{t-1})
- * o_t = sigmoid(W^o x_t + U^o h_{t-1})
+ * i_t = sigmoid(W^i x_t + U^i h_{t-1} + bias0)
+ * f_t = sigmoid(W^f x_t + U^f h_{t-1} + bias1)
+ * o_t = sigmoid(W^o x_t + U^o h_{t-1} + bias2)
  * g_t = tanh(W^g x_t + U^g h_{t-1})
  * c_t = f_t \times c_{t-1} + i_t \times g_t
  * h_t = o_t \times tanh(c_t)
@@ -32,8 +33,10 @@ object LSTMForward extends SpatialApp {
 
   type X = FixPt[TRUE,_16,_16]
 
+//  def sigmoid[T:Type:Num](t: T) = 1.to[T]/(exp(-t) + 1.to[T])
+
   @virtualize
-  def GateForward[T:Type:Num](W: Array[T], x: Array[T], U: Array[T], h: Array[T], bias: Array[T],  mm: Int, nn: Int, N_classes: Int) = {
+  def GateForward[T:Type:Num](W: Array[T], x: Array[T], U: Array[T], h: Array[T], bias: Array[T], mm: Int, nn: Int, N_classes: Int) = {
     val D_h = ArgIn[Int]
     val d = ArgIn[Int]
     val N = ArgIn[Int]
@@ -41,52 +44,97 @@ object LSTMForward extends SpatialApp {
     setArg(d, nn)
     setArg(N, N_classes)
 
-    val W_i = DRAM[T](D_h, d)
-    val x_t = DRAM[T](d, N)
-    val b_i = DRAM[T](D_h, N)
-    val U_i = DRAM[T](D_h, D_h)
+    // Param weights matrices
+    val Wi = DRAM[T](D_h, d)
+    val xt = DRAM[T](d, N)
+    val Ui = DRAM[T](D_h, D_h)
     val h_t_1 = DRAM[T](D_h, N)
+    val bi = DRAM[T](D_h, N)
 
-    val bDh = 5
-    val bd = 5
-    val bN = 1
+    // Result matrix
+    val result = DRAM[T](D_h, N)
 
-    setMem(W_i, W)
-    setMem(x_t, x)
-    setMem(b_i, bias)
-    setMem(U_i, U)
+    // Stepsize for going through N classes
+    val b_N = 1
+
+    // Stepsize for going through hidden size
+    val b_Dh = 5
+
+    // First matmult, col step size
+    val b_Wi_d = 5
+    
+    // Second matmult, col step size
+    val b_Ui_Dh = 2 
+
+    setMem(Wi, W)
+    setMem(xt, x)
+    setMem(bi, bias)
+    setMem(Ui, U)
     setMem(h_t_1, h)
 
     Accel {
-      Sequential.Foreach(D_h by bDh, N by bN) { (i,j) =>
-        val tileBias = SRAM[T](bDh, bN)
-        tileBias load b_i(i::i+bDh, j::j+bN)
+      Foreach(D_h by b_Dh, N by b_N) { (i,j) =>
+        val tile_re = SRAM[T](b_Dh, b_N)
+        val tile_bias = SRAM[T](b_Dh, b_N) 
+        val tile_WTx = SRAM[T](b_Dh, b_N)
+        val tile_UTh = SRAM[T](b_Dh, b_N)
 
-        MemFold(tileBias)(d by bd) { k =>
-          val tileA = SRAM[T](bDh, bd) 
-          val tileB = SRAM[T](bd, bN)
-          val accum = SRAM[T](bDh, bN)
-          Parallel {
-            tileA load W_i(i::i+bDh, k::k+bd)
-            tileB load x_t(k::k+bd, j::j+bN)
+        Parallel {
+          // W_i^Tx 
+          Foreach(d by b_Wi_d) { k =>
+            val tile_Wi = SRAM[T](b_Dh, b_Wi_d) 
+            val tile_xt = SRAM[T](b_Wi_d, b_N)
+            Parallel {
+              tile_Wi load Wi(i::i+b_Dh, k::k+b_Wi_d)
+              tile_xt load xt(k::k+b_Wi_d, j::j+b_N)
+            }
+
+            Foreach(b_Dh by 1, b_N by 1) { (ii,jj) =>
+              val prodWTx = Reduce(Reg[T])(b_Wi_d by 1){ kk => 
+                tile_Wi(ii,kk) * tile_xt(kk,jj)
+              }{_+_}
+
+              val prevWTx = mux(k == 0, 0.to[T], tile_WTx(ii,jj))
+              tile_WTx(ii,jj) = prevWTx + prodWTx.value
+            }
           }
 
-          MemReduce(accum)(bd by 1){ kk =>
-            val tileBias_partial = SRAM[T](bDh, bN)
-            Foreach(bDh by 1, bN by 1){ (ii,jj) =>
-              tileBias_partial(ii,jj) = tileA(ii,kk) * tileB(kk, jj)
+          // U_i^Th_{t-1}
+          Foreach(d by b_Ui_Dh) { k =>
+            val tile_Ui = SRAM[T](b_Ui_Dh, b_Ui_Dh)
+            val tile_ht_1 = SRAM[T](b_Ui_Dh, b_N)
+            Parallel {
+              tile_Ui load Ui(i::i+b_Ui_Dh, k::k+b_Ui_Dh)
+              tile_ht_1 load h_t_1(k::k+b_Ui_Dh, j::j+b_N)
             }
-            tileBias_partial 
-          }{_+_}
-        }{_+_}
 
-        b_i(i::i+bDh, j::j+bN) store tileBias
+            Foreach(b_Dh by 1, b_N by 1) {(ii,jj) =>
+              val prodUTh = Reduce(Reg[T])(b_Ui_Dh by 1){ kk => 
+                tile_Ui(ii,kk) * tile_ht_1(kk,jj)
+              }{_+_}
 
+              val prevUTh = mux(k == 0, 0.to[T], tile_UTh(ii,jj))
+              tile_UTh(ii,jj) = prevUTh + prodUTh.value
+            }
+          }
+          
+          // Load a tile from bias
+          tile_bias load bi(i::i+b_Dh, j::j+b_N)
+        }
 
+        // For the whole tile, reduce the three tiles and send it back to mem
+        Pipe {
+          Foreach(b_Dh by 1, b_N by 1) { (ii, jj) =>
+//            tile_re(ii, jj) = sigmoid(tile_UTh(ii, jj) + tile_WTx(ii, jj) + tile_bias(ii, jj))
+            tile_re(ii, jj) = tile_UTh(ii, jj) + tile_WTx(ii, jj) + tile_bias(ii, jj)
+          } 
 
+          result(i::i+b_Dh, j::j+b_N) store tile_re
+        }
       }
     }
-    getMem(b_i)
+
+    getMem(result)
   }
 
   @virtualize
@@ -102,8 +150,8 @@ object LSTMForward extends SpatialApp {
     val U_i = Array.tabulate(D_h){ j => Array.tabulate(D_h){ i => (i + j).to[X] } } 
     val h_t_1 = Array.tabulate(d) { j => Array.tabulate(N){ i => (i * j).to[X] } } 
 
-    val result = GateForward(W_i.flatten, x_t.flatten, U_i.flatten, h_t_1.flatten,  bias.flatten, D_h, d, N)
+    val gateResult = GateForward(W_i.flatten, x_t.flatten, U_i.flatten, h_t_1.flatten, bias.flatten, D_h, d, N)
 
-    println("result cksum: " + result.map(a => a).reduce{_+_})
+    println("result cksum: " + gateResult.map(a => a).reduce{_+_})
   }
 }
