3e9ef77cb460b6199c33bde80b00184b720411bc
Raghu Prabhakar <raghup17@gmail.com>
2017-06-20 00:04:43 -0700

    Create black boxes for simulation and AWS SRAM. Customize SRAM based on target

--- spatial/core/resources/chiselgen/template-level/fringeHW/SRAM.scala
+++ spatial/core/resources/chiselgen/template-level/fringeHW/SRAM.scala
@@ -6,7 +6,23 @@ import chisel3.util._
 import templates.Utils.log2Up
 
 
-class SRAMVerilog(val w: Int, val d: Int) extends BlackBox(
+class SRAMVerilogSim(val w: Int, val d: Int) extends BlackBox(
+  Map("DWIDTH" -> IntParam(w), "WORDS" -> IntParam(d), "AWIDTH" -> IntParam(log2Up(d))))
+{
+  val addrWidth = log2Up(d)
+  val io = IO(new Bundle {
+    val clk = Input(Clock())
+    val raddr = Input(UInt(addrWidth.W))
+    val waddr = Input(UInt(addrWidth.W))
+    val raddrEn = Input(Bool())
+    val waddrEn = Input(Bool())
+    val wen = Input(Bool())
+    val wdata = Input(UInt(w.W))
+    val rdata = Output(UInt(w.W))
+  })
+}
+
+class SRAMVerilogAWS(val w: Int, val d: Int) extends BlackBox(
   Map("DWIDTH" -> IntParam(w), "WORDS" -> IntParam(d), "AWIDTH" -> IntParam(log2Up(d))))
 {
   val addrWidth = log2Up(d)
@@ -50,21 +66,36 @@ class SRAM(val w: Int, val d: Int) extends Module {
     val rdata = Output(UInt(w.W))
   })
 
-  val mem = Module(new SRAMVerilog(w, d))
-  mem.io.clk := clock
-  mem.io.raddr := io.raddr
-  mem.io.wen := io.wen
-  mem.io.waddr := io.waddr
-  mem.io.wdata := io.wdata
-  mem.io.raddrEn := true.B
-  mem.io.waddrEn := true.B
+  // Customize SRAM here
+  FringeGlobals.target match {
+    case "aws" =>
+      val mem = Module(new SRAMVerilogAWS(w, d))
+      mem.io.clk := clock
+      mem.io.raddr := io.raddr
+      mem.io.wen := io.wen
+      mem.io.waddr := io.waddr
+      mem.io.wdata := io.wdata
+      mem.io.raddrEn := true.B
+      mem.io.waddrEn := true.B
 
-  // Implement WRITE_FIRST logic here
-  // equality register
-  val equalReg = RegNext(io.wen & (io.raddr === io.waddr), false.B)
-  val wdataReg = RegNext(io.wdata, 0.U)
+      // Implement WRITE_FIRST logic here
+      // equality register
+      val equalReg = RegNext(io.wen & (io.raddr === io.waddr), false.B)
+      val wdataReg = RegNext(io.wdata, 0.U)
+      io.rdata := Mux(equalReg, wdataReg, mem.io.rdata)
 
-  io.rdata := Mux(equalReg, wdataReg, mem.io.rdata)
+    case _ =>
+      val mem = Module(new SRAMVerilogSim(w, d))
+      mem.io.clk := clock
+      mem.io.raddr := io.raddr
+      mem.io.wen := io.wen
+      mem.io.waddr := io.waddr
+      mem.io.wdata := io.wdata
+      mem.io.raddrEn := true.B
+      mem.io.waddrEn := true.B
+
+      io.rdata := mem.io.rdata
+  }
 }
 
 /**
