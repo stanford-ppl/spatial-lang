9347279ab2489f8152b70683a42a530b962c990d
kelayamatoz <zhaotian1991713@gmail.com>
2017-07-04 17:28:17 -0700

    vcs simulation failes, scala simulation successful

--- bin/sigmoid/generate_samples.py
+++ bin/sigmoid/generate_samples.py
@@ -1,15 +1,14 @@
-import math
-
-def get_lut_entries(lo, up, n_samples, n_decimals):
+def generate_samples(lo, up, n_samples, n_decimals):
     list = []
     prec = (float(up) - float(lo)) / n_samples
+    print("prec = ", (prec))
     for i in iter(range(n_samples)):
-        list.append(1. / (1. + math.exp(-(i * prec + lo))))
+        list.append(i * prec + lo)
     decimal_str = "%." + str(n_decimals) + "f"
     re_list = [decimal_str % v for v in list]
     return re_list
 
 if __name__ == "__main__":
-    lut_list = get_lut_entries(-32., 32., 128, 10)
-    for v in lut_list:
-        print(v, end=', ')
+    sample_list = generate_samples(-32., 32., 128, 4)
+    for v in sample_list:
+        print(v)
