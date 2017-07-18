02d512384f0be8da97c69986a4045d0b4a5bf647
Ruben Fiszel <ruben.fiszel@epfl.ch>
2017-07-17 20:03:59 -0700

    before scheduling focus

--- spatial/core/src/spatial/interpreter/FixPts.scala
+++ spatial/core/src/spatial/interpreter/FixPts.scala
@@ -16,20 +16,59 @@ trait FixPts extends AInterpreter {
     case FixAdd(EBigDecimal(a), EBigDecimal(b)) =>
       a + b
 
+    case FixSub(EBigDecimal(a), EBigDecimal(b)) =>
+      a - b
+
+    case FixNeg(EBigDecimal(a)) =>
+      -a
+      
+
     case FixPtToFltPt(fixp) =>
       eval[Float](fixp)
 
     case FixEql(EBigDecimal(a), EBigDecimal(b)) =>
       a == b
 
-    case FixLt(EBigDecimal(a), EBigDecimal(b)) =>
-      a < b
+//    case FixLog(EBigDecimal(a)) =>
+//      BigDecimal(Math.log(a.toDouble)) 
 
+//    case FixExp(EBigDecimal(a)) =>
+//      BigDecimal(Math.exp(a.toDouble)) 
+      
+//    case FixSqrt(EBigDecimal(a)) =>
+//      BigDecimal(Math.sqrt(a.toDouble))
+
+//    case FixAcos(EBigDecimal(a)) =>
+//      BigDecimal(Math.acos(a.toDouble))
+      
+//    case FixSin(EBigDecimal(a)) =>
+//      BigDecimal(Math.sin(a.toDouble))
+
+//    case FixCos(EBigDecimal(a)) =>
+//      BigDecimal(Math.cos(a.toDouble))
+
+
+    case FixLt(EBigDecimal(a), EBigDecimal(b)) =>
+      if (a != null && b != null)
+        a < b
+      else
+        null
+
+    case FixLeq(EBigDecimal(a), EBigDecimal(b)) =>
+      if (a != null && b != null)
+        a <= b
+      else
+        null
+      
     case FixDiv(EBigDecimal(a), EBigDecimal(b)) =>
       a / b
 
     case FixMod(EBigDecimal(a), EBigDecimal(b)) =>
       a % b
+
+      
+    case FltMul(EBigDecimal(a), EBigDecimal(b)) =>
+      a * b      
       
     case FixRandom(maxo) =>
       maxo match {
