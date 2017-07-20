54706ee838f656927982165bcc7832f819252d3b
David Koeplinger <dkoeplinger@gmail.com>
2017-07-19 18:02:17 -0700

    Check for errors during DSE

--- spatial/core/src/spatial/dse/DSEThread.scala
+++ spatial/core/src/spatial/dse/DSEThread.scala
@@ -116,10 +116,11 @@ case class DSEThread(
     var i: Int = 0
     var pt: BigInt = start
     while (i < len) {
+      state.resetErrors()
       indexedSpace.foreach{case (domain,d) => domain.set( ((pt / prods(d)) % dims(d)).toInt ) }
 
       val (area, runtime) = evaluate()
-      val valid = area <= capacity
+      val valid = area <= capacity && !state.hadErrors // Encountering errors makes this an invalid design point
 
       array(i) = space.map(_.value).mkString(",") + "," + area.toFile.mkString(",") + "," + runtime + "," + valid
 
