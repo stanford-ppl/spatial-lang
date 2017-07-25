aeb6780251794d52edbd1f9bb6ab2fa1a075dfc0
Matthew Feldman <mattfel@stanford.edu>
2017-07-25 13:30:47 -0700

    fix regfile reset value bugs and unroller bug #190

--- spatial/core/resources/chiselgen/template-level/templates/RegFile.scala
+++ spatial/core/resources/chiselgen/template-level/templates/RegFile.scala
@@ -79,7 +79,11 @@ class ShiftRegFile(val dims: List[Int], val inits: Option[List[Double]], val str
         if (k + 1 < dims.length) {(i / dims.drop(k+1).reduce{_*_}) % dims(k)} else {i % dims(k)}
       }
       when(io.reset) {
-        registers(i) := 0.U(bitWidth.W)
+        if (inits.isDefined) {
+          registers(i) := (inits.get.apply(i)*scala.math.pow(2,fracBits)).toLong.U(bitWidth.W)
+        } else {
+          registers(i) := 0.U(bitWidth.W)            
+        }
       }.elsewhen(io.dump_en) {
         for (i <- 0 until dims.reduce{_*_}) {
           registers(i) := io.dump_data(i)
@@ -123,7 +127,11 @@ class ShiftRegFile(val dims: List[Int], val inits: Option[List[Double]], val str
   } else {
     when(io.reset) {
       for (i <- 0 until dims.reduce{_*_}) {
-        registers(i) := 0.U(bitWidth.W)
+        if (inits.isDefined) {
+          registers(i) := (inits.get.apply(i)*scala.math.pow(2,fracBits)).toLong.U(bitWidth.W)
+        } else {
+          registers(i) := 0.U(bitWidth.W)            
+        }
       }
     }.elsewhen(io.dump_en) {
       for (i <- 0 until dims.reduce{_*_}) {
