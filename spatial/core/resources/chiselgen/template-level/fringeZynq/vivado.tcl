67f18282084beeb041becc011b6a4cf1dadd54c8
raghu <raghup17@gmail.com>
2017-07-25 19:33:29 -0700

    1. Lower clock frequency to 10 MHz 2. Generate timing and utilization reports after synth and par

--- spatial/core/resources/chiselgen/template-level/fringeZynq/vivado.tcl
+++ spatial/core/resources/chiselgen/template-level/fringeZynq/vivado.tcl
@@ -17,12 +17,13 @@ apply_bd_automation -rule xilinx.com:bd_rule:processing_system7 -config {make_ex
 
 
 create_bd_cell -type module -reference Top Top_0
-apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {Master "/processing_system7_0/M_AXI_GP0" Clk "/processing_system7_0/FCLK_CLK0 (50 MHz)" }  [get_bd_intf_pins Top_0/io_S_AXI]
+apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {Master "/processing_system7_0/M_AXI_GP0" Clk "/processing_system7_0/FCLK_CLK0 (10 MHz)" }  [get_bd_intf_pins Top_0/io_S_AXI]
 
 set_property -dict [list CONFIG.PCW_USE_S_AXI_HP0 {1}] [get_bd_cells processing_system7_0]
+set_property -dict [list CONFIG.PCW_FPGA0_PERIPHERAL_FREQMHZ {10}] [get_bd_cells processing_system7_0]
 # set_property -dict [list CONFIG.PCW_USE_S_AXI_ACP {1}] [get_bd_cells processing_system7_0]
 
-apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {Master "/Top_0/io_M_AXI" Clk "/processing_system7_0/FCLK_CLK0 (50 MHz)" }  [get_bd_intf_pins processing_system7_0/S_AXI_HP0]
+apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {Master "/Top_0/io_M_AXI" Clk "/processing_system7_0/FCLK_CLK0 (10 MHz)" }  [get_bd_intf_pins processing_system7_0/S_AXI_HP0]
 #apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {Master "/Top_0/io_M_AXI" Clk "/processing_system7_0/FCLK_CLK0 (50 MHz)" }  [get_bd_intf_pins processing_system7_0/S_AXI_ACP]
 
 validate_bd_design
@@ -38,11 +39,23 @@ update_compile_order -fileset sources_1
 launch_runs synth_1
 wait_on_run synth_1
 
+# Print synth reports on console
+open_run -name synthDesign synth_1
+report_timing_summary -file synth_timing_summary
+report_utilization -packthru -file synth_utilization
+report_ram_utilization -file synth_ram_utilization
+
 launch_runs impl_1
 wait_on_run impl_1
 
 launch_runs impl_1 -to_step write_bitstream
 wait_on_run impl_1
+
+# Reports
+open_run -name implDesign impl_1
+report_timing_summary -file ./par_timing_summary.rpt
+report_utilization -packthru -file ./par_utilization.rpt
+report_ram_utilization -file ./par_ram_utilization.rpt
 
 #Export bitstream
 file copy -force ./project_1/project_1.runs/impl_1/design_1_wrapper.bit ./accel.bit
