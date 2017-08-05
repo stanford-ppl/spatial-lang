410855623b8f6b03a88f3cd021b72601d5e0f8b0
raghu <raghup17@gmail.com>
2017-08-05 14:08:18 -0700

    Vivado script updates to allow instantiating IP cores, bd-less build system

--- spatial/core/resources/chiselgen/template-level/fringeZynq/vivado.tcl
+++ spatial/core/resources/chiselgen/template-level/fringeZynq/vivado.tcl
@@ -1,54 +1,68 @@
-create_project project_1 ./project_1 -part xc7z045ffg900-2
-set_property board_part xilinx.com:zc706:part0:1.3 [current_project]
-#add_files -norecurse {./Top.v ./AXI4LiteToRFBridgeVerilog.v}
-add_files -norecurse [glob *.v]
-update_compile_order -fileset sources_1
-update_compile_order -fileset sim_1
+## Project constants
+set CLOCK_FREQ_MHZ 100
+set CLOCK_FREQ_HZ  [expr $CLOCK_FREQ_MHZ * 1000000]
 
+# Create project to make processing system IP from bd
+create_project ps_project ./ps_project -part xc7z045ffg900-2
+set_property board_part xilinx.com:zc706:part0:1.4 [current_project]
+
+## Create processing system using bd
 create_bd_design "design_1"
 update_compile_order -fileset sources_1
-
-startgroup
 create_bd_cell -type ip -vlnv xilinx.com:ip:processing_system7:5.5 processing_system7_0
-endgroup
-
-
 apply_bd_automation -rule xilinx.com:bd_rule:processing_system7 -config {make_external "FIXED_IO, DDR" apply_board_preset "1" Master "Disable" Slave "Disable" }  [get_bd_cells processing_system7_0]
 
-
-create_bd_cell -type module -reference Top Top_0
-apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {Master "/processing_system7_0/M_AXI_GP0" Clk "/processing_system7_0/FCLK_CLK0 (10 MHz)" }  [get_bd_intf_pins Top_0/io_S_AXI]
-
 set_property -dict [list CONFIG.PCW_USE_S_AXI_HP0 {1}] [get_bd_cells processing_system7_0]
-set_property -dict [list CONFIG.PCW_FPGA0_PERIPHERAL_FREQMHZ {50}] [get_bd_cells processing_system7_0]
-# set_property -dict [list CONFIG.PCW_USE_S_AXI_ACP {1}] [get_bd_cells processing_system7_0]
-
-apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {Master "/Top_0/io_M_AXI" Clk "/processing_system7_0/FCLK_CLK0 (10 MHz)" }  [get_bd_intf_pins processing_system7_0/S_AXI_HP0]
-#apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {Master "/Top_0/io_M_AXI" Clk "/processing_system7_0/FCLK_CLK0 (50 MHz)" }  [get_bd_intf_pins processing_system7_0/S_AXI_ACP]
-
-validate_bd_design
+set_property -dict [list CONFIG.PCW_FPGA0_PERIPHERAL_FREQMHZ $CLOCK_FREQ_MHZ] [get_bd_cells processing_system7_0]
+# validate_bd_design
 save_bd_design
+close_project
 
-make_wrapper -files [get_files ./project_1/project_1.srcs/sources_1/bd/design_1/design_1.bd] -top
-add_files -norecurse ./project_1/project_1.srcs/sources_1/bd/design_1/hdl/design_1_wrapper.v
+## Create a second project to build the design
+create_project project_1 ./project_1 -part xc7z045ffg900-2
+set_property board_part xilinx.com:zc706:part0:1.4 [current_project]
+
+add_files -norecurse [glob *.v]
+
+## Import PS, reset, AXI protocol conversion and word width conversion IP
+import_ip -files [glob *.xci]
+import_ip -files {./ps_project/ps_project.srcs/sources_1/bd/design_1/ip/design_1_processing_system7_0_0/design_1_processing_system7_0_0.xci}
+
+set_property -dict [list CONFIG.CLK.FREQ_HZ $CLOCK_FREQ_HZ] [ get_ips design_1_auto_pc_0]
+set_property -dict [list CONFIG.CLK.FREQ_HZ $CLOCK_FREQ_HZ] [ get_ips design_1_auto_pc_1]
+set_property -dict [list CONFIG.MI_CLK.FREQ_HZ $CLOCK_FREQ_HZ] [ get_ips design_1_auto_ds_0]
+set_property -dict [list CONFIG.SI_CLK.FREQ_HZ $CLOCK_FREQ_HZ] [ get_ips design_1_auto_ds_0]
+
+
+# create_ip -name processing_system7 -vendor xilinx.com -library ip -version 5.5 -module_name processing_system7_0
+#set_property -dict [list CONFIG.PCW_USE_S_AXI_HP0 {1}] [get_ips processing_system7_0]
+#set_property -dict [list CONFIG.PCW_FPGA0_PERIPHERAL_FREQMHZ $CLOCK_FREQ_MHZ] [get_ips processing_system7_0]
+
+#create_ip -name proc_sys_reset -vendor xilinx.com -library ip -version 5.0 -module_name proc_sys_reset_0
+#set_property -dict [list CONFIG.CLOCK.FREQ_HZ $CLOCK_FREQ_HZ] [get_ips proc_sys_reset_0]
+
+
+#import_ip -files {/home/raghu/work/research/spatial/spatial-lang/gen/InOutArg_xilinx_forum/divide_project/project_1/project_1.srcs/sources_1/bd/design_1/ip/design_1_auto_pc_0/design_1_auto_pc_0.xci}
+#import_ip -files {/home/raghu/work/research/spatial/spatial-lang/gen/InOutArg_xilinx_forum/divide_project/project_1/project_1.srcs/sources_1/bd/design_1/ip/design_1_auto_pc_1/design_1_auto_pc_1.xci}
+#import_ip -files {/home/raghu/work/research/spatial/spatial-lang/gen/InOutArg_xilinx_forum/divide_project/project_1/project_1.srcs/sources_1/bd/design_1/ip/design_1_auto_ds_0/design_1_auto_ds_0.xci}
+
+
+# Create Custom IP
+#create_ip -name div_gen -vendor xilinx.com -library ip -version 5.1 -module_name div32
+#set_property -dict [list CONFIG.dividend_and_quotient_width {32} CONFIG.divisor_width {32} CONFIG.remainder_type {Fractional} CONFIG.clocks_per_division {4} CONFIG.fractional_width {32} CONFIG.latency {69}] [get_ips div32]
+#set_property -dict [list CONFIG.ACLK_INTF.FREQ_HZ $CLOCK_FREQ_HZ] [get_ips div32]
+
 update_compile_order -fileset sources_1
-
 set_property top design_1_wrapper [current_fileset]
-update_compile_order -fileset sources_1
 
 set_property STEPS.SYNTH_DESIGN.ARGS.RETIMING true [get_runs synth_1]
+set_property STEPS.SYNTH_DESIGN.ARGS.KEEP_EQUIVALENT_REGISTERS true [get_runs synth_1]
+
 launch_runs synth_1
 wait_on_run synth_1
 
-# Print synth reports on console
-open_run -name synthDesign synth_1
-report_timing_summary -file synth_timing_summary
-report_utilization -packthru -file synth_utilization
-report_ram_utilization -file synth_ram_utilization
-
 launch_runs impl_1
 wait_on_run impl_1
-
 launch_runs impl_1 -to_step write_bitstream
 wait_on_run impl_1
 
