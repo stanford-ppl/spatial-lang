0298a292c40bcf3a561dd582b9c1223165c571f2
raghu <raghup17@gmail.com>
2017-08-08 22:01:08 -0700

    Report both normal and hierarchical utilization

--- spatial/core/resources/chiselgen/template-level/fringeZynq/build/getReports.tcl
+++ spatial/core/resources/chiselgen/template-level/fringeZynq/build/getReports.tcl
@@ -22,13 +22,15 @@ open_project ./project_1/project_1.xpr -part $PART
 
 open_run -name synthDesign synth_1
 report_timing_summary -file ./REPORT_synth_timing_summary.rpt
-report_utilization -packthru -hierarchical -hierarchical_depth 20 -hierarchical_percentages -file ./REPORT_synth_utilization.rpt
+report_utilization -packthru -file ./REPORT_synth_utilization.rpt
+report_utilization -packthru -hierarchical -hierarchical_depth 20 -hierarchical_percentages -file ./REPORT_synth_utilization_hierarchical.rpt
 report_ram_utilization -detail -file ./REPORT_synth_ram_utilization.rpt
 
 # Reports
 open_run -name implDesign impl_1
 report_timing_summary -file ./REPORT_par_timing_summary.rpt
-report_utilization -packthru -hierarchical -hierarchical_depth 20 -hierarchical_percentages -file  ./REPORT_par_utilization.rpt
+report_utilization -packthru -file  ./REPORT_par_utilization.rpt
+report_utilization -packthru -hierarchical -hierarchical_depth 20 -hierarchical_percentages -file  ./REPORT_par_utilization_hierarchical.rpt
 report_ram_utilization -detail -file ./REPORT_par_ram_utilization.rpt
 
 close_project
