32ad63acbd72c448e6b1834e17c783eee89017ef
Matthew Feldman <mattfel@stanford.edu>
2017-08-09 22:30:20 -0700

    updates

--- bin/aws.sh
+++ bin/aws.sh
@@ -3,6 +3,7 @@
 # $1 = flags (--retime, --syncMem, etc)
 # $2 = modifier ("retime", "basic", etc..)
 
+SPATIAL_HOME=`pwd`
 cd ${SPATIAL_HOME}/apps
 ab=`git rev-parse --abbrev-ref HEAD`
 ac=`git rev-parse HEAD`
@@ -27,17 +28,33 @@ screen -d -m -S $2
 
 # annotated_list=(`cat ${SPATIAL_HOME}/apps/src/MachSuite.scala | grep "// Regression" | sed 's/object //g' | sed 's/ extends.*//g'`)
 annotated_list=(
-				"SW" 
-				"MD_Grid" 
-				"GEMM_Blocked"
+				"SW1" 
+				"SW2" 
+				"SW3" 
+				"MD_Grid1" 
+				"MD_Grid2" 
+				"MD_Grid3" 
+				"GEMM_Blocked1"
+				"GEMM_Blocked2"
+				"GEMM_Blocked3"
 				# "SPMV_CRS" 
 				# "PageRank" 
 				# "BlackScholes" 
-				"TPCHQ6" 
-				"AES" 
-				"Kmeans"
-				"GDA"
-				"Sobel"
+				"TPCHQ61" 
+				"TPCHQ62" 
+				"TPCHQ63" 
+				"AES1" 
+				"AES2" 
+				"AES3" 
+				"Kmeans1"
+				"Kmeans2"
+				"Kmeans3"
+				"GDA1"
+				"GDA2"
+				"GDA3"
+				"Sobel1"
+				"Sobel2"
+				"Sobel3"
 								)
 
 				# "LeNet" "DjinnASR" "VGG16"  
@@ -47,7 +64,7 @@ annotated_list=(
 
 for a in ${annotated_list[@]}; do
 
-	CMD="cd /home/mattfel/aws-fpga;source /home/mattfel/aws-fpga/hdk_setup.sh;source /home/mattfel/aws-fpga/sdk_setup.sh;cd $SPATIAL_HOME;rm -rf out_$2_$a;bin/spatial $a --synth --out=out_${2}_${a} $1;cd out_$2_$a;make aws-F1-afi"
+	CMD="export SPATIAL_HOME=${SPATIAL_HOME};cd /home/mattfel/aws-fpga;source /home/mattfel/aws-fpga/hdk_setup.sh;source /home/mattfel/aws-fpga/sdk_setup.sh;cd $SPATIAL_HOME;rm -rf out_$2_$a;bin/spatial $a --synth --out=out_${2}_${a} $1;cd out_$2_$a;make aws-F1-afi"
     # Creates a new screen window with title '$f' in existing screen session
     screen -S $2 -X screen -t $a
 
