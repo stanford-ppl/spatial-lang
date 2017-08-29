5a120afe83819372c005b14d78d5ee14f18dfed0
Yaqi Zhang <hsbzyq@gmail.com>
2017-08-29 02:53:37 -0700

    add hardware counter for number of load and store

--- spatial/core/resources/chiselgen/template-level/fringeHW/MAGCore.scala
+++ spatial/core/resources/chiselgen/template-level/fringeHW/MAGCore.scala
@@ -640,6 +640,14 @@ class MAGCore(
     ctr.io.out
   }
 
+  def addWhen(en: UInt, value: UInt) = {
+    val ff = Module(new FF(32))
+    ff.io.init := 15162342.U
+    ff.io.enable := en
+    ff.io.in := ff.io.out + value
+    ff.io.out
+  }
+
   var dbgCount = 0
   val signalLabels = ListBuffer[String]()
   def connectDbgSignal(sig: UInt, label: String = "") {
@@ -675,10 +683,15 @@ class MAGCore(
     //}
   //}
 
-  //connectDbgSignal(numCommandsCtr.io.out, "Num DRAM Commands")
-  //connectDbgSignal(numReadCommandsCtr.io.out, "Read Commands")
-  //connectDbgSignal(numWriteCommandsCtr.io.out, "Write Commands")
+  connectDbgSignal(numCommandsCtr.io.out, "Num DRAM Commands")
+  connectDbgSignal(numReadCommandsCtr.io.out, "Read Commands")
+  connectDbgSignal(numWriteCommandsCtr.io.out, "Write Commands")
 
+  connectDbgSignal(getCounter(io.enable & io.dram.cmd.ready & io.dram.cmd.valid & io.dram.cmd.bits.isSparse), "Number of Sparse requests")
+  connectDbgSignal(getCounter(io.enable & io.dram.cmd.ready & io.dram.cmd.valid & io.dram.cmd.bits.isSparse & ~io.dram.cmd.bits.isWr), "Sparse reads")
+  connectDbgSignal(getCounter(io.enable & io.dram.cmd.ready & io.dram.cmd.valid & io.dram.cmd.bits.isSparse & io.dram.cmd.bits.isWr), "Sparse writes")
+  connectDbgSignal(addWhen(io.enable & io.dram.cmd.ready & io.dram.cmd.valid & ~io.dram.cmd.bits.isSparse & ~io.dram.cmd.bits.isWr, io.dram.cmd.bits.size), "Sum of size (#bursts) of dense loads")
+  connectDbgSignal(addWhen(io.enable & io.dram.cmd.ready & io.dram.cmd.valid & ~io.dram.cmd.bits.isSparse & io.dram.cmd.bits.isWr, io.dram.cmd.bits.size), "Sum of size (#bursts) of dense stores")
   //// Count number of commands issued per stream
   //(0 until numStreams) foreach { case i =>
     //val signal = "cmd" + (if (i < loadStreamInfo.size) "load" else "store") + s"stream $i"
