d998b73acea5b9681a7d774101e9f7a4702dc110
Matthew Feldman <mattfel@stanford.edu>
2017-10-19 17:27:07 -0700

    move gsheet starter until after clock defined

--- bin/synth_regression.sh
+++ bin/synth_regression.sh
@@ -2,9 +2,19 @@
 
 if [[ $1 = "Zynq" ]]; then
 	export CLOCK_FREQ_MHZ=125
+	# Prep the spreadsheet
+	cd ${REGRESSION_HOME}
+	tid=`python3 tid.py "$hash" "$timestamp" "Zynq"`
+	echo $tid > ${REGRESSION_HOME}/data/tid
 elif [[ $1 = "F1" ]]; then
 	export CLOCK_FREQ_MHZ=125
+	# Prep the spreadsheet
+	cd ${REGRESSION_HOME}
+	tid=`python3 ${REGRESSION_HOME}/../zynq/tid.py "$hash" "$timestamp" "F1"`
+	echo $tid > ${REGRESSION_HOME}/data/tid
 fi
+
+
 
 cd ${REGRESSION_HOME}/spatial/spatial-lang
 
