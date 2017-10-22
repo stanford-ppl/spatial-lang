a004513032fa2c94fb0382142c742cc6731db6b8
Matthew Feldman <mattfel@stanford.edu>
2017-10-22 12:40:04 -0700

    add ultrascale model

--- bin/synth_regression.sh
+++ bin/synth_regression.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-if [[ $1 = "Zynq" ]]; then
+if [[ $1 = "zynq" ]]; then
 	export CLOCK_FREQ_MHZ=125
 	# Prep the spreadsheet
 	cd ${REGRESSION_HOME}
 	tid=`python3 tid.py "$hash" "$timestamp" "Zynq"`
 	echo $tid > ${REGRESSION_HOME}/data/tid
-elif [[ $1 = "F1" ]]; then
+elif [[ $1 = "aws" ]]; then
 	export CLOCK_FREQ_MHZ=125
 	# Prep the spreadsheet
 	cd ${REGRESSION_HOME}
-	tid=`python3 ${REGRESSION_HOME}/../zynq/tid.py "$hash" "$timestamp" "F1"`
+	tid=`python3 ${REGRESSION_HOME}/../zynq/tid.py "$hash" "$timestamp" "AWS"`
 	echo $tid > ${REGRESSION_HOME}/data/tid
 fi
 
@@ -20,6 +20,6 @@ cd ${REGRESSION_HOME}/spatial/spatial-lang
 
 if [[ $1 = "Zynq" ]]; then
 	bin/regression 4 nobranch Zynq Dense Sparse
-elif [[ $1 = "F1" ]]; then
-	bin/regression 4 nobranch F1 Dense Sparse
+elif [[ $1 = "AWS" ]]; then
+	bin/regression 4 nobranch AWS Dense Sparse
 fi
