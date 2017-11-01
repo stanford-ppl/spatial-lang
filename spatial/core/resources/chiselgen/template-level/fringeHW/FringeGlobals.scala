c49fd0ffaebcb1266a33664835d3d41f22cff0b2
Matt Feldman <matt@pilot.ai>
2017-11-01 12:56:15 -0700

    fix bigIP set for zcu

--- spatial/core/resources/chiselgen/template-level/fringeHW/FringeGlobals.scala
+++ spatial/core/resources/chiselgen/template-level/fringeHW/FringeGlobals.scala
@@ -18,7 +18,7 @@ object FringeGlobals {
   def target = _target
   def target_= (value: String): Unit = {
     bigIP = value match {
-      case "zynq" => new fringeZynq.bigIP.BigIPZynq()
+      case "zynq" | "zcu" => new fringeZynq.bigIP.BigIPZynq()
       case "aws" => new fringeAWS.bigIP.BigIPAWS()
       case "de1soc" => new fringeDE1SoC.bigIP.BigIPDE1SoC()
       case _ => new fringe.bigIP.BigIPSim()
