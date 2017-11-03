ae800d5ef26cefae55a222cf2e790334be311a53
Matthew Feldman <mattfel@stanford.edu>
2017-11-03 16:21:00 -0700

    fix bd auto bug

--- spatial/core/resources/chiselgen/template-level/fringeZynq/build/bdproject.tcl
+++ spatial/core/resources/chiselgen/template-level/fringeZynq/build/bdproject.tcl
@@ -35,9 +35,9 @@ switch $TARGET {
     # set_property -dict [list CONFIG.PCW_FPGA1_PERIPHERAL_FREQMHZ {250} CONFIG.PCW_EN_CLK1_PORT {1}] [get_bd_cells processing_system7_0]
     apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {Master "/zynq_ultra_ps_e_0/M_AXI_HPM0_FPD" Clk "/zynq_ultra_ps_e_0/pl_clk0 ($CLOCK_FREQ_MHZ MHz)" }  [get_bd_intf_pins Top_0/io_S_AXI]
     # Try using LPD instead
-    set_property -dict [list CONFIG.PSU__USE__M_AXI_GP0 {0} CONFIG.PSU__USE__M_AXI_GP2 {1}] [get_bd_cells zynq_ultra_ps_e_0]
-    delete_bd_objs [get_bd_intf_nets zynq_ultra_ps_e_0_M_AXI_HPM0_FPD]
-    connect_bd_intf_net [get_bd_intf_pins zynq_ultra_ps_e_0/M_AXI_HPM0_LPD] -boundary_type upper [get_bd_intf_pins ps8_0_axi_periph/S00_AXI]
+    # set_property -dict [list CONFIG.PSU__USE__M_AXI_GP0 {0} CONFIG.PSU__USE__M_AXI_GP2 {1}] [get_bd_cells zynq_ultra_ps_e_0]
+    # delete_bd_objs [get_bd_intf_nets zynq_ultra_ps_e_0_M_AXI_HPM0_FPD]
+    # connect_bd_intf_net [get_bd_intf_pins zynq_ultra_ps_e_0/M_AXI_HPM0_FPD] -boundary_type upper [get_bd_intf_pins ps8_0_axi_periph/S00_AXI]
     
     # create_bd_cell -type ip -vlnv xilinx.com:ip:proc_sys_reset:5.0 proc_sys_reset_fclk1
     # connect_bd_net [get_bd_pins proc_sys_reset_fclk1/ext_reset_in] [get_bd_pins zynq_ultra_ps_e_0/pl_resetn0]
