3c3f013912d2f12616d27db061fa08dcf0f21147
Yaqi Zhang <hsbzyq@gmail.com>
2017-11-14 17:28:08 -0800

    fix warning when PIR_HOME is not set even when pir is not enabled

--- spatial/core/src/spatial/SpatialConfig.scala
+++ spatial/core/src/spatial/SpatialConfig.scala
@@ -65,8 +65,7 @@ class SpatialConfig extends argon.core.Config {
   var enableSynth: Boolean = _
   var enablePIR: Boolean = _
   var enablePIRSim: Boolean = false
-  lazy val PIR_HOME: String = sys.env.getOrElse("PIR_HOME", {Report.error("Please set the PIR_HOME environment variable."); sys.exit()})
-  var pirsrc: String = s"$PIR_HOME/pir/apps/src"
+  var pirsrc: Option[String] = None 
 
   var enableRetiming: Boolean = _
 
