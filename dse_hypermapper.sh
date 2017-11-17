f369d6dc1443e266a28fd3202c9e27ed49da34d1
David Koeplinger <dkoeplinger@gmail.com>
2017-11-16 20:37:47 -0800

    dse_hypermapper.sh

--- dse_hypermapper.sh
+++ dse_hypermapper.sh
@@ -2,8 +2,16 @@
 
 benchmarks=("BlackScholes" "DotProduct" "GDA" "Kmeans" "MatMult_outer"  "OuterProduct" "Sobel" "SW" "TPCHQ6")
 
+iters=(0,1,2,3,4,5,6,7,8,9)
+
 for benchmark in "${benchmarks[@]}"
 do
-    echo "bin/spatial ${benchmark} --experiment --threads 16"
-    bin/spatial ${benchmark} --hypermapper --threads 16 2>&1 | tee ${benchmark}.log
+
+    for i in "${iters[@]}"
+    do
+        echo "bin/spatial ${benchmark} --experiment --threads 16"
+        bin/spatial ${benchmark} --hypermapper --threads 16 2>&1 | tee ${benchmark}.log
+        mv dse_hm/generic_data_array_RS_1000_max_AL_5_execution_0.csv "dse_hm/${benchmark}_data_${i}.csv"
+        mv dse_hm/generic_pareto_RS_1000_max_AL_5_execution_0.csv "dse_hm/${benchmark}_pareto_${i}.csv"
+    done
 done
