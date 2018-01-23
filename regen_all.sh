8a12f6fdac1a3e3ef50a9ba64270bc5d119f6d86
kelayamatoz <zhaotian1991713@gmail.com>
2018-01-22 21:48:21 -0800

    commit with argon

--- regen_all.sh
+++ regen_all.sh
@@ -1,11 +1,11 @@
 declare -a arr=("BasicDRAMReads" "DRAM0Stores" "DRAM2Stores" "DRAMLoadFirstEle" "DRAMLoads" "DRAMRWTest" "DRAMWriteTest" "InOutArg" "OneBurstOneLoad" "OneBurstOneLoadRegFile" "OneBurstOneStore" "OneBurstOneStoreArgs" "OneBurstTwoStore" "SRAMRWTest" "TwoBurst" "OneBurstTwoLoad")
 # CMD=./bin/arria10_debuggers/regen_cpp.sh
-# CMD=regen.sh
+CMD=regen.sh
 for i in "${arr[@]}"
 do
   echo "$i"
-  # screen -S $i -dm bash ${CMD} $i
+  screen -S $i -dm bash ${CMD} $i
   # scp ./gen/${i}/${i}.tar.gz root@arria10:~/BasicMemTests/
   # bash ${CMD} $i
-  scp ./1burst.csv root@arria10:~/BasicMemTests/$i/
+  # scp ./1burst.csv root@arria10:~/BasicMemTests/$i/
 done
