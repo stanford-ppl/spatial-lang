5d811ea1fbd9849de8126dd8bc40d76c56d3f0dc
kelayamatoz <zhaotian1991713@gmail.com>
2018-01-22 16:37:37 -0800

    updated regen

--- regen_all.sh
+++ regen_all.sh
@@ -1,8 +1,9 @@
-declare -a arr=("BasicDRAMReads" "DRAM0Stores" "DRAM2Stores" "DRAMLoadFirstEle" "DRAMLoads" "DRAMRWTest" "DRAMWriteTest" "InOutArg" "OneBurstOneLoad" "OneBurstOneStore" "OneBurstTwoStore" "OneBurstOneStoreArgs" "SRAMRWTest")
-CMD=./bin/arria10_debuggers/regen_cpp.sh
+declare -a arr=("BasicDRAMReads" "DRAM0Stores" "DRAM2Stores" "DRAMLoadFirstEle" "DRAMLoads" "DRAMRWTest" "DRAMWriteTest" "InOutArg" "OneBurstOneLoad" "OneBurstOneLoadRegFile" "OneBurstOneStore" "OneBurstOneStoreArgs" "OneBurstTwoStore" "SRAMRWTest" "TwoBurst" "OneBurstTwoLoad")
+# CMD=./bin/arria10_debuggers/regen_cpp.sh
+CMD=regen.sh
 for i in "${arr[@]}"
 do
   echo "$i"
-  # screen -S $i -dm bash ${CMD} $i
-  bash ${CMD} $i
+  screen -S $i -dm bash ${CMD} $i
+  # bash ${CMD} $i
 done
