f67fd3e1e76a1780f56d0d50c1cc69e77621e33c
kelayamatoz <zhaotian1991713@gmail.com>
2018-03-05 18:51:28 -0800

    added Arria10 as a target

---
+++ spatial/core/src/spatial/targets/Arria10.scala
@@ -0,0 +1,19 @@
+package spatial.targets
+
+import spatial.models.altera._
+import spatial.models._
+
+object Arria10 extends AlteraDevice {
+  import AlteraDevice._
+  val name = "Arria10"
+  def burstSize = 512
+
+  case object VideoCamera extends Bus {def length = 32}
+  case object VGA extends Bus {def length = 32}
+
+
+  // FIXME: No models for Arria10 yet
+  protected def makeAreaModel: AreaModel       = new StratixVAreaModel
+  protected def makeLatencyModel: LatencyModel = new StratixVLatencyModel
+  def capacity: Area = AreaMap(ALMs->262400, Regs->524800, DSPs->1963, BRAM->2567, Channels->13)
+}
