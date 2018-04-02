5dae85e098036c865aa2039cfb57507829b743e1
David Koeplinger <dkoeplinger@gmail.com>
2018-04-02 14:45:04 -0700

    update

--- spatial/core/src/spatial/analysis/ControlSignalAnalyzer.scala
+++ spatial/core/src/spatial/analysis/ControlSignalAnalyzer.scala
@@ -36,7 +36,6 @@ trait ControlSignalAnalyzer extends SpatialTraversal {
   var inInnerLoop: Boolean = false                        // Is the innermost loop iterator an inner controller?
 
   var localMems: List[Exp[_]] = Nil
-  private var localReads: List[Exp[_]] = Nil
   var metapipes: List[Exp[_]] = Nil
   var streampipes: List[Exp[_]] = Nil
   var streamLoadCtrls: List[Exp[_]] = Nil // Pops
@@ -118,7 +117,7 @@ trait ControlSignalAnalyzer extends SpatialTraversal {
     inds.zip(factors).foreach{case (i,f) => parFactorOf(i) = f }
     inds.zip(countersOf(cchain)).foreach{case (i,c) => ctrOf(i) = c }
     controller.foreach{ctrl => inds.foreach{i => ctrlOf(i) = ctrl }}
-    unrollFactors = factors.lastOption.toList +: unrollFactors
+    unrollFactors = factors.toList +: unrollFactors
     loopIterators = loopIterators ++ inds
     inInnerLoop = isInnerControl(blkToCtrl(blk)) // This version of the method is only called for loops
 
@@ -143,7 +142,6 @@ trait ControlSignalAnalyzer extends SpatialTraversal {
   }
 
   def addReader(reader: Exp[_], ctrl: Ctrl) = instrument("addReader"){
-    localReads ::= reader
     if (isInnerControl(ctrl))
       appendReader(reader, ctrl)
     else
@@ -154,7 +152,7 @@ trait ControlSignalAnalyzer extends SpatialTraversal {
     val LocalWriter(writes) = writer
     val Def(writeDef) = writer
     writes.foreach{case (mem,value,addr,en) =>
-      writersOf(mem) = (writer,ctrl) +: writersOf(mem)      // (5)
+      writersOf(mem) = ((writer,ctrl)) +: writersOf(mem)    // (5)
       writtenIn(ctrl) = mem +: writtenIn(ctrl)              // (10)
       //val isAccumulatingWrite = writeDef.inputs.filterNot(_ == mem).exists{_.dependsOn(mem, curScope) }
       //isAccum(mem) = isAccum(mem) || isAccumulatingWrite
@@ -293,7 +291,6 @@ trait ControlSignalAnalyzer extends SpatialTraversal {
     getDef(node).foreach{d =>
       val inputs = d.expInputs
       memDepsOf(node) = (inputs intersect localMems).toSet ++ inputs.flatMap(in => memDepsOf(in))
-      readDepsOf(node) = (inputs intersect localReads).toSet ++ inputs.flatMap{in => readDepsOf(in)}
       dbgs(s"memory dependencies of $node: " + memDepsOf(node).mkString(","))
     }
   }
@@ -461,10 +458,12 @@ trait ControlSignalAnalyzer extends SpatialTraversal {
       isInnerAccum(accum) = isInnerControl(lhs)
 
     case OpMemReduce(en,cchainMap,cchainRed,accum,map,ldRes,ldAcc,reduce,store,_,_,rV,itersMap,itersRed) =>
-      visitBlk((lhs,0), itersMap, cchainMap) { visitBlock(map) }
-      visitBlk((lhs,1), itersRed, cchainRed) { visitBlock(ldAcc) }
-      visitBlk((lhs,2), itersRed, cchainRed) { visitBlock(ldRes) }
-      visitBlk((lhs,3), itersRed, cchainRed) { visitBlock(reduce) }
+      visitBlk((lhs,0), itersMap, cchainMap) {
+        visitBlock(map)
+        visitBlk((lhs,1), itersRed, cchainRed){ visitBlock(ldRes) }
+        visitBlk((lhs,3), itersRed, cchainRed){ visitBlock(reduce) }
+      }
+      visitBlk((lhs,2), itersRed, cchainRed){ visitBlock(ldAcc) }
       visitBlk((lhs,4), itersRed, cchainRed) { visitBlock(store) }
 
       isAccum(accum) = true
