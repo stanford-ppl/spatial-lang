9ce0401ccbac3fae2ad4ca5b766d1fd160f165df
Yaqi Zhang <hsbzyq@gmail.com>
2018-04-17 15:44:43 -0700

    update scala trace gen with dram offset. update make file to change permission on run.sh

--- spatial/core/src/spatial/codegen/scalagen/ScalaGenTrace.scala
+++ spatial/core/src/spatial/codegen/scalagen/ScalaGenTrace.scala
@@ -9,32 +9,59 @@ import java.nio.file._
 
 trait ScalaGenTrace extends ScalaGenControl with ScalaGenMemories {
 
-  lazy val tracesPath = s"${config.genDir}${config.sep}traces"
+  lazy val tracesPath = src"${config.genDir}${config.sep}traces"
+
+  var emitedCurrOffset = false
+  def emitCurrOffset = {
+    if (!emitedCurrOffset) {
+      emit(src"var currentOffset = 0")
+      emitedCurrOffset = true
+    }
+  }
 
   def getTraceFile(lhs:Any) = {
-    s"""new java.io.File("${tracesPath}${config.sep}${lhs}.trace")"""
+    src"""new java.io.File("${tracesPath}${config.sep}${lhs}.trace")"""
   }
 
   def getTrace(lhs:Any) = {
-    s"""traces.getOrElseUpdate("${lhs}", new java.io.PrintWriter(${getTraceFile(lhs)}))"""
+    src"""traces.getOrElseUpdate("${lhs}", new java.io.PrintWriter(${getTraceFile(lhs)}))"""
+  }
+
+  def emitTrace(lhs:Any, dram:Exp[_], signal:Any) = {
+    emit(src"""${getTrace(lhs)}.write($signal + ${dram}_offset + "\n")""")
   }
 
   override protected def emitNode(lhs: Sym[_], rhs: Op[_]): Unit = rhs match {
     case Hwblock(func,isForever) =>
       if (spatialConfig.enableTrace) {
-        emit(s"val traces = scala.collection.mutable.Map[String, java.io.PrintWriter]()")
-        Files.createDirectories(Paths.get(tracesPath))
+        emit(src"val traces = scala.collection.mutable.Map[String, java.io.PrintWriter]()")
+        val dir = new java.io.File(tracesPath)
+        if (dir.exists) {
+          dir.listFiles.foreach(_.delete)
+        } else {
+          Files.createDirectories(Paths.get(tracesPath))
+        }
       }
       super.emitNode(lhs, rhs)
       if (spatialConfig.enableTrace) {
-        emit(s"traces.foreach{ case (name, trace) => trace.close()}")
-        emit(s"traces.clear")
+        emit(src"traces.foreach{ case (name, trace) => trace.close()}")
+        emit(src"traces.clear")
       }
+
+    case op@DRAMNew(dims,zero) =>
+      super.emitNode(lhs, rhs)
+      val elementsPerBurst = spatialConfig.target.burstSize / op.bT.length
+      val dramSizeWord = src"""(${dims.map(quote).mkString("*")} + $elementsPerBurst)"""
+      val bytesPerWord = op.bT.length / 8 + (if (op.bT.length % 8 != 0) 1 else 0)
+      val dramSizeBytes = src"""$dramSizeWord * $bytesPerWord"""
+      emitCurrOffset
+      emit(src"""val ${lhs}_offset = currentOffset""")
+      emit(src"""currentOffset += $dramSizeBytes""")
 
     case e@FringeDenseLoad(dram,cmdStream,dataStream) =>
       if (spatialConfig.enableTrace) {
         open(src"$cmdStream.foreach{cmd => ")
-          emit(s"""${getTrace(lhs)}.write(cmd.offset + "\\n")""")
+          emitTrace(lhs, dram, "cmd.offset")
         close("}")
       }
       super.emitNode(lhs, rhs)
@@ -42,7 +69,7 @@ trait ScalaGenTrace extends ScalaGenControl with ScalaGenMemories {
     case e@FringeDenseStore(dram,cmdStream,dataStream,ackStream) =>
       if (spatialConfig.enableTrace) {
         open(src"$cmdStream.foreach{cmd => ")
-          emit(s"""${getTrace(lhs)}.write(cmd.offset + "\\n")""")
+          emitTrace(lhs, dram, "cmd.offset")
         close("}")
       }
       super.emitNode(lhs, rhs)
@@ -50,7 +77,7 @@ trait ScalaGenTrace extends ScalaGenControl with ScalaGenMemories {
     case e@FringeSparseLoad(dram,addrStream,dataStream) =>
       if (spatialConfig.enableTrace) {
         open(src"$addrStream.foreach{addr => ")
-          emit(s"""${getTrace(lhs)}.write(addr + "\\n")""")
+          emitTrace(lhs, dram, "addr")
         close("}")
       }
       super.emitNode(lhs, rhs)
@@ -58,8 +85,7 @@ trait ScalaGenTrace extends ScalaGenControl with ScalaGenMemories {
     case e@FringeSparseStore(dram,cmdStream,ackStream) =>
       if (spatialConfig.enableTrace) {
         open(src"$cmdStream.foreach{cmd => ")
-          emit(s"""${getTrace(s"${lhs}_addr")}.write(cmd._2 + "\\n")""")
-          emit(s"""${getTrace(s"${lhs}_data")}.write(cmd._1 + "\\n")""")
+          emitTrace(lhs, dram, "cmd._2")
         close("}")
       }
       super.emitNode(lhs, rhs)
