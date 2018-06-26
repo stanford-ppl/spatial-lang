c6f2a758765ef5c4a74ba22ce40b01e5c1a5ecb1
Yaqi Zhang <hsbzyq@gmail.com>
2018-06-26 12:25:46 -0700

    update apps

--- spatial/core/src/spatial/codegen/pirgen/PIRGenOp.scala
+++ spatial/core/src/spatial/codegen/pirgen/PIRGenOp.scala
@@ -34,17 +34,15 @@ trait PIRGenOp extends PIRCodegen {
                 emit(s"val ${quote(lhs)} = ${quote(x)} // $rhs (Same Type. No op)")
               case FixPtType(`s1`,`i1`,f2) =>
                 emit(s"val ${quote(lhs)} = ${quote(x)} // $rhs (fraction difference. Ignored on plasticine)")
-              case FixPtType(s2,`i1`,f2) =>
-                emit(s"val ${quote(lhs)} = ${quote(x)} // $rhs unsigned <-> signed.") //TODO: hardware support?
               case LongType() =>
                 warn(s"Plasticine only support 32 bit wordwidth. FixConvert to LongType. Use single precision instead ${lhs.ctx}")
                 dbg(s"Plasticine only support 32 bit wordwidth. FixConvert to Use single precision instead")
                 emit(s"val ${quote(lhs)} = ${quote(x)} // $rhs")
               case FixPtType(s2, i2, f2) =>
                 val bitWidth = i2 + f2
-                if (bitWidth != 32) {
-                  warn(s"Plasticine only support 32 bit wordwidth. FixConvert to ${lhs.tp}, bitWidth=$bitWidth ${lhs.ctx}")
-                  dbg(s"Plasticine only support 32 bit wordwidth. FixConvert to ${lhs.tp}, bitWidth=$bitWidth ${lhs.ctx}")
+                if (bitWidth > 32) {
+                  warn(s"Plasticine only support up to 32 bit wordwidth. FixConvert to ${lhs.tp}, bitWidth=$bitWidth ${lhs.ctx}")
+                  dbg(s"Plasticine only support up to 32 bit wordwidth. FixConvert to ${lhs.tp}, bitWidth=$bitWidth ${lhs.ctx}")
                 }
                 /*
                  * [ s1 | i1 |   f1 ]
@@ -59,11 +57,12 @@ trait PIRGenOp extends PIRCodegen {
                 val iMaskStr = iMask.mkString
                 val fMaskStr = fMask.mkString
                 emit(s"// ${quote(lhs)} = $rhs x.tp=${x.tp} {")
-                emit(LhsSym(lhs, Some("int1")), s"""OpDef(op=BitAnd, inputs=List(${quote(x)}, Const("$iMaskStr")))""", rhs)
-                emit(LhsSym(lhs, Some("int2")), s"OpDef(op=$op, inputs=List(${quote(lhs)}_int1, Const($sftamt)))", rhs)
-                emit(LhsSym(lhs, Some("frac1")), s"""OpDef(op=BitAnd, inputs=List(${quote(x)}, Const("$fMaskStr")))""", rhs)
-                emit(LhsSym(lhs, Some("frac2")), s"OpDef(op=$op, inputs=List(${quote(lhs)}_frac1, Const($sftamt)))", rhs)
-                emit(lhs, s"OpDef(op=BitOr, inputs=List(${quote(lhs)}_int2, ${quote(lhs)}_frac2))", rhs)
+                //emit(LhsSym(lhs, Some("int1")), s"""OpDef(op=BitAnd, inputs=List(${quote(x)}, Const("$iMaskStr")))""", rhs)
+                //emit(LhsSym(lhs, Some("int2")), s"OpDef(op=$op, inputs=List(${quote(lhs)}_int1, Const($sftamt)))", rhs)
+                //emit(LhsSym(lhs, Some("frac1")), s"""OpDef(op=BitAnd, inputs=List(${quote(x)}, Const("$fMaskStr")))""", rhs)
+                //emit(LhsSym(lhs, Some("frac2")), s"OpDef(op=$op, inputs=List(${quote(lhs)}_frac1, Const($sftamt)))", rhs)
+                //emit(lhs, s"OpDef(op=BitOr, inputs=List(${quote(lhs)}_int2, ${quote(lhs)}_frac2))", rhs)
+                emit(lhs, s"""OpDef(op=$op, inputs=List(${quote(x)}, Const("$sftamt")))""", rhs)
                 emit(s"// }")
             }
           case VectorApply(vec, idx) =>
