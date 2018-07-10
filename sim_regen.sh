dbbaefb1478cb2825b53d714ee0491cf8f10f4cd
kelayamatoz <zhaotian1991713@gmail.com>
2018-07-10 11:58:22 -0700

    mod sim and synth to decouple sim and synth stages

--- sim_regen.sh
+++
@@ -1,3 +0,0 @@
-rm -rf gen/$1
-bin/spatial $1 --sim
-bash $1.sim | tee sim.log
--- synth_regen.sh
+++
@@ -1,6 +0,0 @@
-CMD="make zcu"
-
-rm -rf gen/$1
-bin/spatial $1 --synth --retime --instrumentation
-echo "Working on synthesizing"
-cd gen/$1 && screen -S $1 -dm ${CMD}
